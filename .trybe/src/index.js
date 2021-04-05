93f317684c8a1b61793382a2484d58c5a5c808a8
nevesfreire <nando.neves@gmail.com>
2021-04-05 17:02:45 -0300

    1 and 2

--- .trybe/src/index.js
+++ .trybe/src/index.js
@@ -4,36 +4,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.json());
+const fs = require('fs')
 
-const crushes = [
-    {
-      "name": "Madonna",
-      "age": 62,
-      "id": 1,
-      "date": { "datedAt": "23/10/2020", "rate": 5 }
-    },
-    {
-      "name": "Cyndi Lauper",
-      "age": 67,
-      "id": 2,
-      "date": { "datedAt": "23/10/2020", "rate": 5 }
-    },
-    {
-      "name": "Kendrick Lamar",
-      "age": 33,
-      "id": 3,
-      "date": { "datedAt": "23/10/2020", "rate": 5 }
-    },
-    {
-      "name": "Tom Holland",
-      "age": 24,
-      "id": 4,
-      "date": { "datedAt": "23/10/2020", "rate": 5 }
+app.get('/crush', (req, res) => {
+    console.log ('entrou no get')
+    const crushList = fs.readFileSync('./crush.json')
+
+    if (crushList) {
+        return (res.status(200).json(crushList))
     }
-  ];
+    if (crushList.lenght === 0) {
+        return res.status(200).json([])
+    }
 
-app.patch('/crush', (req, res) => {
-    
 })
 
 app.listen(port, () => console.log(`rolê tá acontecendo na porta ${port}`));
--- index.js
+++ index.js
@@ -6,10 +6,35 @@ app.use(bodyParser.json());
 
 const SUCCESS = 200;
 const PORT = '3000';
+const fs = require('fs')
+
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', (req, res) => {
+  const crushList = JSON.parse(fs.readFileSync('./crush.json', 'utf8'))
+  if (crushList.length > 0) {
+    return res.status(200).json(crushList);  
+  }
+  if (crushList.lenght === 0) {
+    return res.status(200).json([])
+  }
+})
+
+app.get('/crush/:idtofind', (req, res) => {
+  console.log('entrou no :id')
+
+  const { idtofind } = req.params;
+
+  const crushList = JSON.parse(fs.readFileSync('./crush.json', 'utf8'))
+  const crushIndex = crushList.findIndex(({ id }) => id == idtofind);
+  if (crushIndex === -1) {
+    res.status(404).send({"message": "Crush não encontrado"})
+  }
+  res.send(crushList[crushIndex])
+})
 
 app.listen(PORT, () => { console.log('Online'); });
