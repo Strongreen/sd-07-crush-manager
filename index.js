ea184eaff0a7be9a57f472b83c65e75433514db5
Flavio Oliveira <flavsoliver@gmail.com>
2021-04-05 20:54:25 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +8,19 @@ app.use(bodyParser.json());
 const SUCCESS = 200;
 const PORT = '3000';
 
+const crushList = 'crush.json';
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+app.get('/crush', async (req, res) => {
+  const crush = await fs.promises.readFile(crushList);
+  if (crush) {
+    return res.status(200).json(JSON.parse(crush));
+  }
+  return res.status(200).json([]);
 });
 
 app.listen(PORT, () => { console.log('Online'); });
