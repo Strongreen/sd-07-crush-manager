0dd9306b1937e871df32579bb52a0a0dc9b07361
Vitor Rodrigues <vitor_rc@outlook.com>
2021-04-05 16:01:10 -0300

    Terceiro requisito

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
 const NOT_FOUND = 404;
+const BAD_REQUEST = 400;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -16,7 +18,7 @@ app.get('/', (_request, response) => {
 
 const getJSON = () => JSON.parse(fs.readFileSync('./crush.json', 'utf-8'));
 
-app.get('/crush', (req, res) => {
+app.get('/crush', (_req, res) => {
   const crushJSON = getJSON();
   res.status(SUCCESS).json(crushJSON);
 });
@@ -29,6 +31,27 @@ app.get('/crush/:id', (req, res) => {
   } else {
     res.status(NOT_FOUND).json({ message: 'Crush não encontrado' });
   }
+});
+
+const generateToken = (length) => crypto.randomBytes(length).toString('hex');
+
+app.post('/login', (req, res) => {
+  console.log('po');
+  const { email, password } = req.body;
+  const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (!email) {
+    res.status(BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
+  } else if (!regex.test(email)) {
+    res.status(BAD_REQUEST).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (!password) {
+    res.status(BAD_REQUEST).json({ message: 'O campo "password" é obrigatório' });
+  } else if (password.length < 6) {
+    res.status(BAD_REQUEST).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  const tokenLength = 8;
+  res.status(SUCCESS).json({ token: generateToken(tokenLength) });
 });
 
 app.listen(PORT, () => { console.log('Online'); });
