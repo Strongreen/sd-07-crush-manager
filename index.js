779fe8ef6193db37888ebcbac4e92b55d3c7623d
Vitor Rodrigues <vitor_rc@outlook.com>
2021-04-05 15:26:48 -0300

    Segundo requisito

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
+const NOT_FOUND = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -13,9 +14,21 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
+const getJSON = () => JSON.parse(fs.readFileSync('./crush.json', 'utf-8'));
+
 app.get('/crush', (req, res) => {
-  const crushJSON = fs.readFileSync('./crush.json', 'utf-8');
-  res.status(SUCCESS).json(JSON.parse(crushJSON));
-})
+  const crushJSON = getJSON();
+  res.status(SUCCESS).json(crushJSON);
+});
+
+app.get('/crush/:id', (req, res) => {
+  const { id } = req.params;
+  const filteredCrush = getJSON().find((crush) => crush.id === Number(id));
+  if (filteredCrush) {
+    res.status(SUCCESS).json(filteredCrush);
+  } else {
+    res.status(NOT_FOUND).json({ message: 'Crush não encontrado' });
+  }
+});
 
 app.listen(PORT, () => { console.log('Online'); });
