26105bd42f8521170e9f2e71a9fb5b0055f8195a
Danwhat <daanwhat@gmail.com>
2021-04-05 16:20:54 -0300

    eu amo o list

--- middleware/checkLogin.js
+++ middleware/checkLogin.js
@@ -1,13 +1,18 @@
+const emailCheck = (email) => {
+  const emailRegex = /^\S+@\S+$/;
+  if (!email || email.length === 0) {
+    return { message: 'O campo "email" é obrigatório' };
+  }
+  if (!emailRegex.test(email)) {
+    return { message: 'O "email" deve ter o formato "email@email.com"' };
+  }
+};
+
 module.exports = (req, res, next) => {
   const { email, password } = req.body;
   // check email
-  const emailRegex = /^\S+@\S+$/;
-  if (!email || email.length === 0) {
-    return res.status(400).send({ message: 'O campo "email" é obrigatório' });
-  }
-  if (!emailRegex.test(email)) {
-    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
+  const emailCheckReturn = emailCheck(email);
+  if (emailCheckReturn) return res.status(400).send(emailCheckReturn);
   // check password
   if (!password || password.length === 0) {
     return res.status(400).send({ message: 'O campo "password" é obrigatório' });
