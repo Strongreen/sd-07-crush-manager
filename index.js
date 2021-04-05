9ca2ef3866314d1f8516d74c4d5cd5e9680cb4e3
CarolSi <a.carolg1@gmail.com>
2021-04-05 17:29:06 -0300

    requisitos 1 e 2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +12,28 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+const readCrushFile = async () => {  
+  try {
+  const content = await fs.readFile(path.resolve(__dirname, '.', 'crush.json'));
+  return JSON.parse(content.toString('utf-8'));
+  } catch (error) {
+    return error;
+  } 
+};
+
+app.get('/crush', async (req, res) => {
+  const crushes = await readCrushFile();
+  res.status(200).send(crushes);  
+});
+
+app.get('/crush/:id', async (req, res) => {
+  const crushes = await readCrushFile();
+  const { id } = req.params;
+  const filteredCrush = crushes.find((crush) => crush.id === Number(id));
+  if (!filteredCrush) return res.status(404).send({ message: 'Crush não encontrado' });  
+  res.status(200).send(filteredCrush);  
 });
 
 app.listen(PORT, () => { console.log('Online'); });
