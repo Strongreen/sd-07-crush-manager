46d9308b82f88b51880dcfedee74511e699dc6b0
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-04-05 13:54:25 -0300

    requisito 02: metodo get para mostrar crush especifico com id do params

--- routes/crush.js
+++ routes/crush.js
@@ -7,4 +7,17 @@ app.get('/', (_req, res) => {
   res.status(200).send(data);
 });
 
+app.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const crushById = data.find((element) => element.id === parseInt(id, 10));
+
+  if (!crushById) {
+    res.status(404).send({
+      message: 'Crush não encontrado',
+    });
+  }
+
+  res.status(200).send(crushById);
+});
+
 module.exports = app;
