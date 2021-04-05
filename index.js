345006237721c6ac3086ba539823c35c5dbb0068
ericvini <ericcobain3@gmail.com>
2021-04-05 14:56:28 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +12,22 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+const readCrushFile = async () => {
+  const content = await fs.readFile(path.resolve(__dirname, '.', 'crush.json'));
+  return JSON.parse(content.toString('utf-8'));
+};
+
+app.get('/crush', async (_req, res) => {
+  const result = await readCrushFile();
+  const emptyArray = [];
+  if (result.length === 0) res.status(200).send({ emptyArray });
+  res.status(200).send(result);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
 });
 
 app.listen(PORT, () => { console.log('Online'); });
