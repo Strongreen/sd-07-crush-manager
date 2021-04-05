f50c5a6514998ffd546fbe52bf6214b592c8ddfe
nevesfreire <nando.neves@gmail.com>
2021-04-05 18:00:52 -0300

    lint

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ app.use(bodyParser.json());
 
 const SUCCESS = 200;
 const PORT = '3000';
-const fs = require('fs')
-
+const fs = require('fs');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -15,26 +14,32 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/crush', (req, res) => {
-  const crushList = JSON.parse(fs.readFileSync('./crush.json', 'utf8'))
+  const crushList = JSON.parse(fs.readFileSync('./crush.json', 'utf8'));
   if (crushList.length > 0) {
     return res.status(200).json(crushList);  
   }
   if (crushList.lenght === 0) {
-    return res.status(200).json([])
+    return res.status(200).json([]);
   }
-})
+});
 
 app.get('/crush/:idtofind', (req, res) => {
-  console.log('entrou no :id')
+  console.log('entrou no :id');
 
   const { idtofind } = req.params;
 
-  const crushList = JSON.parse(fs.readFileSync('./crush.json', 'utf8'))
-  const crushIndex = crushList.findIndex(({ id }) => id == idtofind);
+  const crushList = JSON.parse(fs.readFileSync('./crush.json', 'utf8'));
+  const crushIndex = crushList.findIndex(({ id }) => id === idtofind);
   if (crushIndex === -1) {
-    res.status(404).send({"message": "Crush não encontrado"})
+    res.status(404).send({ message: 'Crush não encontrado' });
   }
-  res.send(crushList[crushIndex])
-})
+  res.send(crushList[crushIndex]);
+});
+
+// app.post('/login', (req, res) => {
+// const {email, password} = req.body;
+
+// // crypto
+// })
 
 app.listen(PORT, () => { console.log('Online'); });
