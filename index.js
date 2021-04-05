23ca178f6063d38777572e4d73a412ff2baaf94e
rodrigoderkian <derkianpromo@gmail.com>
2021-04-05 16:19:25 -0300

    req1

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
-const PORT = '3000';
+const PORT = "3000";
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
+app.get("/", (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.listen(PORT, () => { console.log('Online'); });
+app.get("/crush", async (req, res) => {
+  const data = await fs.readFile("./crush.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log(`Não foi possível ler o arquivo. Erro: ${err.message}`);
+    }
+    return res.status(SUCCESS).send(data);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Online");
+});
