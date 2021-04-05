01a1353b4e0e5d3e6041faa972f727c036f4d6ce
TIAGO <tiagoesdrasvieira@gmail.com>
2021-04-05 17:06:26 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 
 const fs = require('fs');
 
-const crush = './crush.json';
+const crushFile = './crush.json';
 
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
+const NOTFOUND = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -16,9 +17,26 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.get('/crush', (_req, res) => {
-  const crushs = JSON.parse(fs.readFileSync(crush), 'utf-8');  
+const readFile = (file) => {
+  const response = JSON.parse(fs.readFileSync(file), 'utf-8');
+  return response;
+};
+
+app.get('/crush', (_req, res) => {  
+  const crushs = readFile(crushFile);
   res.status(SUCCESS).send(crushs);
+});
+
+app.get('/crush/:id', (req, res) => {
+  const crushs = readFile(crushFile);
+  const { id } = req.params;
+  const crushId = crushs.find((c) => c.id === parseInt(id, 10));
+  if (crushId) {
+    res.status(SUCCESS).send(crushId);
+  }
+  res.status(NOTFOUND).send({
+    message: 'Crush não encontrado',
+  });
 });
 
 app.listen(PORT, () => { console.log('Online'); });
