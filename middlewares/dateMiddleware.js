1a4c1a058d727bd8a6feee582b7e3ca684255808
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-04-05 16:44:43 -0300

    Req 5

--- middlewares/dateMiddleware.js
+++ middlewares/dateMiddleware.js
@@ -2,17 +2,16 @@ const status = require('../helpers/status');
 
 const dateMiddleware = (request, response, next) => {
   const { date } = request.body;
-  if (!date || !date.datedAt || !date.rate) {
+  if (!date || !date.datedAt || date.rate === undefined) {
  return response.status(status.BAD_REQUEST).json(
     { message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' },
   ); 
-} 
-
-  if (date.rate < 1) {
- return response.status(status.BAD_REQUEST).json(
-    { message: 'O campo "rate" deve ser um inteiro de 1 à 5' },
-  ); 
 }
+if (date.rate < 1) {
+  return response.status(status.BAD_REQUEST).json(
+     { message: 'O campo "rate" deve ser um inteiro de 1 à 5' },
+   ); 
+ }
   next();
 };
 
--- routers/crush.js
+++ routers/crush.js
@@ -28,15 +28,15 @@ crushRouter.get('/:id', async (request, response) => {
 
   // console.log((parseFloat(crushId)));
   const crushData = await readCrushFile();
-  
+
   const resultData = crushData.find(({ id }) => id === parseFloat(crushId));
   // console.log(result);
-  
+
   if (!resultData) {
- return response.status(status.NOT_FOUND).json(
-    { message: 'Crush não encontrado' },
-    ); 
-}
+    return response.status(status.NOT_FOUND).json(
+      { message: 'Crush não encontrado' },
+    );
+  }
   response.status(status.SUCCESS).json(resultData);
 });
 
@@ -51,37 +51,25 @@ crushRouter.post('/', async (request, response) => {
   const crushData = await readCrushFile();
   const newCrush = { id: !crushData.length ? 1 : crushData.length + 1, ...request.body };
   const newCrushData = [...crushData, newCrush];
-  console.log(newCrushData);
   await writeCrushFile(newCrushData);
   response.status(status.CREATED).json(newCrush);
 });
 
-/* crushRouter.put('/:id', (request, response) => {
+crushRouter.put('/:id', async (request, response) => {
   const { id: crushId } = request.params;
-  // const { id: crushId, ...reqs } = request.body;
+  const { ...reqs } = request.body;
+  const crushData = await readCrushFile();
+  const newCrush = { id: parseFloat(crushId), ...reqs };
+  const removeResult = await crushData.filter(({ id }) => id !== parseFloat(crushId));
+  const newCrushData = [...removeResult, newCrush];
 
-  /*   const message = validInputs(name, age, date);
-    if (message !== 'xablau') return response.status(status.BAD_REQUEST).json({ message }); */
-  // let crushData = [];
-  // console.log(result);
-  // console.log(typeof crushData);
-  // console.log(crushData);
-  // const crushData = readCrushFile();
-  // console.log(crushData);
-  // const array = [];
-  // array.push(crushData);
-  // const result = Object.values({ crushData });
-  // const newCrushData = { ...request.body };
-  // console.log(array);
-  // const removeResult = crushData.filter((e) => e.id !== result.id);
-  // console.log(removeResult);
-  // writeCrushFile(newCrushData);
-  // console.log(readCrushFile());
-/*  const result = array.find(({ id }) => id === parseFloat(crushId));
-  if (!result) return response.status(status.NOT_FOUND).json({ message: 'Crush não encontrado' });
-  response.status(status.SUCCESS).json(newCrushData);
+  await writeCrushFile(newCrushData);
+// console.log(readCrushFile());
+// const result = crushData.find(({ id }) => id === parseFloat(crushId));
+// if (!result) return response.status(status.NOT_FOUND).json({ message: 'Crush não encontrado' });
+response.status(status.SUCCESS).json(newCrush);
 });
- */
+
 crushRouter.delete('/:id', (request, response) => {
   const { id: crushId } = request.params;
 
