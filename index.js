62b9bc73c667ec75e4519c2a2b6021de4470a973
MoisesSantana <moisaant@gmail.com>
2021-04-05 14:34:42 -0300

    initial commit req 1 and 2

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
-app.use(bodyParser.json());
-
 const SUCCESS = 200;
-const PORT = '3000';
+const NOT_FIND = 404;
+const ERROR = 500;
+const PORT = 3000;
+
+app.use(express.json());
+
+const crush = './crush.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+async function returnData() {
+  const data = await fs.promises.readFile(crush, 'utf-8');
+  return JSON.parse(data);
+}
+
+app.get('/crush', async (_req, res) => {
+  try {
+    const data = await returnData();
+    
+    return res.status(SUCCESS).json(data);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get('/crush/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await returnData();
+    const getCrushById = data.filter((crush) => crush.id === Number(id));
+    if (getCrushById.length < 1)
+      return res.status(NOT_FIND).json({ message: 'Crush não encontrado' });
+
+    return res.status(SUCCESS).json(getCrushById[0]);
+  } catch (err) {
+    console.log(err)
+  }
+});
+
+app.post('/login', (req, res) => {
+  
 });
 
 app.listen(PORT, () => { console.log('Online'); });
