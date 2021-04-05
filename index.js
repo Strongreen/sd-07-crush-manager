e5debde7949214a1fafef29bbdb7538e600c39ba
Lfocosi <luccafocosi@gmail.com>
2021-04-05 16:37:19 -0300

    start

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
-
 const SUCCESS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+// getAllCrushs
+app.get('/crush', (_request, response) => {
+  const content = JSON.parse(fs.readFileSync(`${__dirname}/crush.json`));
+  response.status(SUCCESS).json([...content]);
+});
+// getCrushById
+app.get('/crush/:id', (request, response) => {
+  const { id } = request.params;
+  const obj = { status: 404, response: { message: 'Crush não encontrado' } };
+  const content = JSON.parse(fs.readFileSync(`${__dirname}/crush.json`));
+  content.forEach((crush) => {
+    if (crush.id === id) {
+      obj.status = SUCCESS;
+      obj.response = crush;
+    }
+  });
+  response.status(obj.status).json(obj.response);
 });
 
 app.listen(PORT, () => { console.log('Online'); });
