d50deadc9158d786fd4196345dcf704d8eb50e4a
TIAGO <tiagoesdrasvieira@gmail.com>
2021-04-05 16:40:41 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fs = require('fs');
+
+const crush = './crush.json';
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -10,6 +14,11 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+app.get('/crush', (_req, res) => {
+  const crushs = JSON.parse(fs.readFileSync(crush), 'utf-8');  
+  res.status(SUCCESS).send(crushs);
 });
 
 app.listen(PORT, () => { console.log('Online'); });
