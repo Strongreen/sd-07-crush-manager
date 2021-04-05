2e63e20801fa2affdb081fc1ceffc7731939e9aa
Breno Alves <brenoalv3ss@gmail.com>
2021-04-05 18:51:14 -0300

    Crush Manager Project

---
+++ routes/crushId.js
@@ -0,0 +1,10 @@
+/* const express = require('express');
+const data = require('../data/crush.json');
+
+const app = express();
+
+const SUCCESS = 200;
+
+app.get('/crush/:id', (_request, response) => {
+  
+}); */
