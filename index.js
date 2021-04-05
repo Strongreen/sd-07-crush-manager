32c8d223c9bddb98807499e06891c19ce8004f1b
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-04-05 20:48:55 -0300

    teste

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const crushes = require('./crush.json');
+// const crushes = require('./crush.json');
 const crushRoutes = require('./routes/crush.js');
 
 const app = express();
