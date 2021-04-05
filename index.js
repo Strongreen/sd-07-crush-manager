022c39a0cb556b6df6099287216c035bf7c17f5a
Breno Alves <brenoalv3ss@gmail.com>
2021-04-05 20:52:12 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
+const NOT_FOUND = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -17,6 +18,16 @@ const getJSON = () => JSON.parse(fs.readFileSync('./crush.json', 'utf-8'));
 app.get('/crush', (_request, response) => {
   const data = getJSON();
   response.status(SUCCESS).json(data);
+});
+
+app.get('/crush/:id', (request, response) => {
+  const { id } = request.params;
+  const filter = getJSON().find((crush) => crush.id === Number(id));
+  if (filter) {
+    response.status(SUCCESS).json(filter);
+  } else {
+    response.status(NOT_FOUND).json({ message: 'Crush não encontrado' });
+  }
 });
 
 app.listen(PORT, () => { console.log('Online'); });
