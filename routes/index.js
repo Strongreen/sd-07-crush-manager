5a65e88b7f3979516ae252589c26089e15e82c45
w27M <marcos.1998226@gmail.com>
2021-04-05 16:24:45 -0300

    requisito 1

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const data = require('../crush.json');
+const fs = require('fs');
 
-const app = express();
+const router = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send(data);
+const File = 'crush.json';
+
+router.get('/', (req, res) => {
+    const file = fs.readFileSync(File);
+    const dataCrush = file.toString('utf8');
+    const crushData = JSON.parse(dataCrush);
+
+   if (crushData.length !== 0) {
+       res.status(200).send(crushData);
+    } 
+    res.status(200).send([]);
 });
 
-module.exports = app;
+module.exports = router;
