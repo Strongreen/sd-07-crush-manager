42e18ae08a8c3e324ce50b65cae91ce23aceb27d
renatomak <renato.mark.silva@gmail.com>
2021-04-05 17:40:19 -0300

    Requisito 4 - segunda parte

--- routers/crush.js
+++ routers/crush.js
@@ -6,6 +6,7 @@ const data = require('../crush.json');
 const router = express.Router();
 const SUCCESS = 200;
 const NOTFOUND = 404;
+const BADREQUESTERROR = 400;
 
 router.get('/', (req, res) => {
   if (data.length === 0) return res.status(SUCCESS).send([]);
@@ -21,7 +22,17 @@ router.get('/:id', (req, res) => {
   return res.status(SUCCESS).send(crush);
 });
 
-router.post('/', rescue(async (req, res) => {
+const checkedName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) return res.status(BADREQUESTERROR).send({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) { 
+    return res.status(BADREQUESTERROR)
+    .send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+router.post('/', checkedName, rescue(async (req, res) => {
   try {
     const { name, age, date } = req.body;
     data.push({ name, age, date });
