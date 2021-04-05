e40c79fe9bb70a3f108b9fe706d0a821d7ecb1dc
Carol Bezerra <carol.arbe@gmail.com>
2021-04-05 17:21:28 -0300

    Req. 1 tentativa 4

--- crush.js
+++ crush.js
@@ -1,8 +1,8 @@
-const fs = require('fs', 'utf8');
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const crushFile = fs.readFileSync('./crush.json');
+const crushFile = fs.readFileSync('./crush.json', 'utf8');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,6 +14,24 @@ app.get('/', (_req, res) => {
     res.status(200).send(crushFile);
   }
 });
+
+// function isValidId(id) {
+//   // return crushFile.find((item) => item.id === id);
+//   JSON.parse(crushFile);
+//   console.log(crushFile);
+//   console.log(typeof crushFile);
+//   console.log(typeof JSON.parse(crushFile));
+//   console.log(Array.isArray(crushFile));
+// }
+
+// app.get('/:id', (req, res) => {
+//   const isId = isValidId(req.params.id);
+//   if (isId) {
+//     res.status(200).send(isId);
+//   } else {
+//     res.status(404).send({ message: 'Crush não encontrado' });
+//   }
+// });
 
 app.use((err, _req, res, _next) => {
   res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
