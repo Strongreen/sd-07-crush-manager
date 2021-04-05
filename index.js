c9fdcab945cd2bce80a82693deceb5ae5bda1ae8
julioKauer <juliokauer@gmail.com>
2021-04-05 15:55:29 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
 const PORT = '3000';
+const DATAPATH = './crush.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+app.get('/crush', (_request, response) => {
+  const crushes = JSON.parse(fs.readFileSync(DATAPATH));
+  response.status(SUCCESS).json(crushes);
 });
 
 app.listen(PORT, () => { console.log('Online'); });
