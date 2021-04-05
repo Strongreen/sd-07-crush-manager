5069d3c8cc909bbc6f79e25cc1e9708718687c5f
Lucas Andrade <lucas@Mac-mini-de-Fattoria.local>
2021-04-05 19:30:00 -0300

    1 2 e 3

--- middlewares/emailRegex.js
+++ middlewares/emailRegex.js
@@ -1,6 +1,5 @@
 const emailRegex = (req, res, next) => {
     const { email } = req.body;
-    console.log(email, !/\S+@\S+\.\S+/.test(email))
     if (!/\S+@\S+\.\S+/.test(email)) {
  return res.status(400).send({
         message: 'O "email" deve ter o formato "email@email.com"',
