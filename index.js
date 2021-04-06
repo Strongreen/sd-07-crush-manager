8217a31a52771f41e2e9edd6582e9ecfd30d31fd
Melissa Gomes <mel.ge171201@gmail.com>
2021-04-06 12:10:18 -0300

    testando primeiro req

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 const SUCCESS = 200;
 const PORT = '3000';
@@ -13,3 +13,54 @@ app.get('/', (_request, response) => {
 });
 
 app.listen(PORT, () => { console.log('Online'); });
+
+const crushList = './crush.json';
+const crushId = '/crush/:id';
+
+app.get(crushList, async (_req, res) => {
+  const myCrush = await fs.readFile(crushList, 'utf8');
+  const newListCrush = JSON.parse(myCrush);
+  res.status(SUCCESS).json(newListCrush);
+});
+
+app.get(crushId, async (req, res) => {
+  const myCrush = await fs.readFile(crushList, 'utf8');
+  const id = Number(req.params.id);
+  const crushJson = JSON.parse(myCrush);
+  const idCrush = await crushJson.find((crush) => crush.id === id);
+  if (idCrush) {
+    return res.status(SUCCESS).json(idCrush);
+  }
+  return res.status(404).json({
+    message: 'Crush não encontrado',
+  });
+});
+
+/* async function test () {
+  const result = await read();
+  console.log(result[0]);
+} */
+
+// test();
+
+/* const emailValidation = (email) => {
+  const rejexForEmail = /^[^\s@]+@[^\s@]+$/;
+  return rejexForEmail.test(email);
+}
+
+const passwordValidation = (password) => {
+  const rejexForPassword = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  return rejexForPassword.test(password);
+}
+
+const token = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if( authorization.length !== 16 ) {
+     return res.status(401).json({
+      message: 'Token inválido',
+    });
+  }
+
+  next();
+} */ 
