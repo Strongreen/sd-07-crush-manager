3d2f1e747d224d38ed759f09baf8645903441936
w27M <marcos.1998226@gmail.com>
2021-04-06 16:35:55 -0300

    requisito 3

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -4,15 +4,15 @@ const validationMiddleware = (req, res, next) => {
     const emailTest = (/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(email);
 
     if (!email) {
- res.status(400).send({ message: 'O campo "email" é obrigatório' });
+       res.status(400).send({ message: 'O campo "email" é obrigatório' });
     } else if (!emailTest) {
- res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"',
+       res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"',
         });
     } else if (!password) {
         res.status(400).send({ message: 'O campo "password" é obrigatório',
         });
     } else if (passwordToString.length < 6) {
- res.status(400).send({
+        res.status(400).send({
             message: 'O "password" deve ter pelo menos 6 caracteres',
         });
     } 
