f224604a3b0c01629f0cce75ea56d6c3141d57d5
Bruno <bruno_grb@hotmail.com>
2021-04-06 19:08:48 -0300

    desafio4 pronto

--- routes/crushes.js
+++ routes/crushes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const middlewaresTokens = require('../middlewares/middlewaresTokens');
 
 const app = express();
 
@@ -21,6 +22,50 @@ app.get('/:id', (req, res) => {
     res.status(404).send({
       message: error.message,
     });
+  }
+});
+
+async function validateName(name) {
+  if (!name) throw new Error('O campo "name" é obrigatório');
+  if (name.length < 3) throw new Error('O "name" deve ter pelo menos 3 caracteres');
+}
+
+async function validateAge(age) {
+  if (!age) throw new Error('O campo "age" é obrigatório');
+  if (age < 18) throw new Error('O crush deve ser maior de idade');
+}
+
+function verifyCorretDate(date) {
+  const reForDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  if (!reForDate.test(date)) throw new Error('O campo "datedAt" deve ter o formato "dd/mm/aaaa"');
+}
+
+async function validateDate(date) {
+  if (!date || !date.rate || !date.datedAt) {
+    throw new Error('O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios');
+  }
+  await verifyCorretDate(date.datedAt);
+  if (date.rate < 1 || date.rate > 5) {
+    throw new Error('O campo "rate" deve ser um inteiro de 1 à 5');
+  }
+}
+
+app.use(middlewaresTokens);
+
+app.post('/', async (req, res) => {
+  const { name, age, date } = req.body;
+  try {
+    await validateName(name);
+    await validateAge(age);
+    await validateDate(date);
+    const data = JSON.parse(fs.readFileSync(`${__dirname}/../crush.json`, 'utf8'));
+    const size = data.length;
+    const newCrush = { name: name, id: size + 1, age: age, date: date };
+    data.push(newCrush);
+    await fs.promises.writeFile(`${__dirname}/crush.json`, JSON.stringify(data));
+    res.status(201).send(newCrush);
+  } catch (error) {
+    res.status(400).send({ message: error.message }); 
   }
 });
 
