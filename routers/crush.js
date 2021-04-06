492696dafabf6eeabf6ef97a9928c9cd78f0465e
Lucas <lafdesouza2002@outlook.com>
2021-04-06 11:30:49 -0300

    Alguns desafios

--- routers/crush.js
+++ routers/crush.js
@@ -22,7 +22,7 @@ router.get('/:id', (req, res) => {
   const id = Number(req.params.id);
   const object = data.find((value) => value.id === id);
   if (!object) {
- res.status(404).send({
+ return res.status(404).send({
     message: 'Crush não encontrado',
   }); 
 }
