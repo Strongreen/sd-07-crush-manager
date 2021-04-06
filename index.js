ca9f149b2787034e635aa1cf8aadf04ff3cae4dd
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-04-06 17:45:50 -0300

    adicionando middleware de erro geral

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 
 const crush = require('./routes/crush');
 const login = require('./routes/login');
+const MIDDLEWARES = require('./middlewares/index');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/login', login);
 app.use('/crush', crush);
+
+app.use(MIDDLEWARES.errorMiddleware);
 
 const SUCCESS = 200;
 const PORT = '3000';
--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const authMiddleware = require('./auth');
 const nameMiddleware = require('./name');
 const ageMiddleware = require('./age');
 const dateMiddleware = require('./date');
+const errorMiddleware = require('./error');
 
 module.exports = {
   authMiddleware,
   nameMiddleware,
   ageMiddleware,
   dateMiddleware,
+  errorMiddleware,
 };
