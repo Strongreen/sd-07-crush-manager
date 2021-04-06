cbc54ab7bee4baeb3d2956563484973d6cc0d2ee
rodrigoderkian <derkianpromo@gmail.com>
2021-04-06 09:49:38 -0300

    refatorando e deixando assincrono

--- index.js
+++ index.js
@@ -8,27 +8,22 @@ app.use(bodyParser.json());
 const SUCCESS = 200;
 const PORT = '3000';
 
-const crushs = JSON.parse(fs.readFileSync('./crush.json', 'utf8'));
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.get('/crush', (req, res) => {
-  if (crushs.length === 0) {
-    console.log(crushs);
-    res.status(200).send([]);
-  }
-  console.log(crushs);
-  res.status(200).send(crushs);
+app.get('/crush', async (req, res) => {
+  const crushs = JSON.parse(await fs.promises.readFile('./crush.json', 'utf8'));
+  res.status(SUCCESS).send(crushs);
 });
 
-app.get('/crush/:id', (req, res) => {
+app.get('/crush/:id', async (req, res) => {
   const { id: reqId } = req.params;
+  const crushs = JSON.parse(await fs.promises.readFile('./crush.json', 'utf8'));
   const crush = crushs.find(({ id }) => id.toString() === reqId);
   if (crush) {
-    res.status(200).send(crush);
+    res.status(SUCCESS).send(crush);
   }
   return res.status(404).send({
     message: 'Crush não encontrado',
