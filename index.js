6dd0690d5b640d2727be6390ed363bd84433c0cb
Flavio Oliveira <flavsoliver@gmail.com>
2021-04-06 17:55:51 -0300

    requisito 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const dataCrush = require('./crush.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,12 +16,25 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.get('/crush', async (req, res) => {
+app.get('/crush', async (_req, res) => {
   const crush = await fs.promises.readFile(crushList);
   if (crush) {
     return res.status(200).json(JSON.parse(crush));
   }
   return res.status(200).json([]);
 });
+
+app.get('/crush/:id', (req, res) => {
+  const { id: reqId } = req.params;
+  const crushId = dataCrush.find(({ id }) => id === Number(reqId));
+  if (crushId) {
+    return res.status(200).json(crushId);
+  }
+  return res.status(404).json({
+    message: 'Crush não encontrado',
+  });
+});
+
+app.get('/crush', (_req, res) => res.status(200).json(dataCrush));
 
 app.listen(PORT, () => { console.log('Online'); });
