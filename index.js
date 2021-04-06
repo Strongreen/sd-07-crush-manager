57a730b97f24d5e2b1d914308d386c4c519efda4
hvenses <hvenses@gmail.com>
2021-04-06 19:26:56 -0300

    Commitando requisito 4

--- index.js
+++ index.js
@@ -24,11 +24,16 @@ const fileReader = async () => {
   return JSON.parse(file);
 };
 
+// ---------- Função para escrever no arquivo --------
+// const fileWriter = async (newUser) => {
+//   await fs.writeFile(data, JSON.stringify(newUser)); 
+// };
+
 // ----------- Função que valida email ---------------
 // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 function validateEmail(email) {
-  const reg = /\S+@\S+\.\S+/;
-  return reg.test(email);
+  const emailReg = /\S+@\S+\.\S+/;
+  return emailReg.test(email);
 }
 
 // ----------- Função que valida senha ---------------
@@ -37,7 +42,85 @@ function validatePassword(password) {
   if (password.toString().length >= maximumSize) return password;
 }
 
-// 1 ------- Crie o endpoint GET /crush ---------------
+// ------------ Middleware que valida token ---------------
+function validateTokenMiddleware(request, response, next) {
+  const { authorization } = request.headers;
+  if (!authorization) {
+    return response.status(401).json({
+      message: 'Token não encontrado',
+    });
+  }
+  if (authorization.length !== 16) {
+    return response.status(401).json({
+      message: 'Token inválido',
+    });
+  }
+  next();
+}
+
+// ------------- Middleware que valida name -------------
+function validateNameMiddleware(request, response, next) {
+  const { name } = request.body;
+  const minSize = 3;
+  if (!name) {
+    return response.status(400).json({
+      message: 'O campo "name" é obrigatório',
+    });
+  }
+  if (name.length < minSize) {
+    return response.status(400).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+  next();
+}
+
+// ------------- Middleware que valida age -------------
+function validateAgeMiddleware(request, response, next) {
+  const { age } = request.body;
+  const minAge = 18;
+  if (!age) {
+    return response.status(400).json({
+      message: 'O campo "age" é obrigatório',
+    });
+  }
+  if (age < minAge) {
+    return response.status(400).json({
+      message: 'O crush deve ser maior de idade',
+    });
+  }
+  next();
+}
+
+// ------------- Middleware que valida date -------------
+function validateDateMiddleware(request, response, next) {
+  // Regex dd/mm/aaaa: https://www.regextester.com/99555
+  const { date } = request.body;
+  const dateReg = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  if (!date || !date.rate || !date.datedAt) {
+    return response.status(400).json({
+    message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  }
+  if (!(dateReg.test(date.datedAt))) {
+    return response.status(400).json({
+      message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+  next();
+}
+
+// ------------- Middleware que valida date -------------
+function validateRateMiddleware(request, response, next) {
+  const { date } = request.body;
+  if (date.rate < 1 || date.rate > 5 || date.rate !== parseInt(date.rate, 0)) {
+    return response.status(400).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+  next();
+}
+
+// 1 --------- Crie o endpoint GET /crush ---------------
 app.get('/crush', async (_request, response) => {
   const crushList = await fileReader();
   if (!crushList) {
@@ -73,4 +156,22 @@ app.post('/login', (request, response) => {
     return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   return response.status(SUCCESS).json({ token });
+});
+
+// 4 ---------- Crie o endpoint POST /crush ---------------
+app.use(validateTokenMiddleware, validateNameMiddleware, 
+validateAgeMiddleware, validateDateMiddleware, validateRateMiddleware);
+app.post('/crush', async (request, response) => {
+  const { name, age, date } = request.body;
+  const nextCrush = await fileReader();
+  const crush = {
+    id: nextCrush.length + 1,
+    name,
+    age: parseInt(age, 0),
+    date: {
+      datedAt: date.datedAt,
+      rate: date.rate,
+    },
+  };
+  return response.status(201).json(crush);
 });
