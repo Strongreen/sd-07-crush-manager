74b846b3caf4213bba01754b21f793385b0160b1
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-04-06 16:21:58 -0300

    First commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const crushes = require('./crush.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,10 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+app.get('/crush', (request, response) => {
+  response.status(200).json(crushes);
 });
 
 app.listen(PORT, () => { console.log('Online'); });
