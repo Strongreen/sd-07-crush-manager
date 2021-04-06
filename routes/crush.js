3c8ee1d1a40ebebc4f338ff7d407f905e98dc86e
Renato Silva <renatosdev@gmail.com>
2021-04-06 16:10:15 -0300

    desafio2

--- routes/crush.js
+++ routes/crush.js
@@ -6,21 +6,20 @@ const app = express();
 const readCrush = async () => JSON.parse(await fs.readFile('./crush.json', 'utf8'));
 
 app.get('/', async (req, res) => {
-  const result = await readCrush();
-  return res.status(200).send(result);
+  const dataCrush = await readCrush();
+  return res.status(200).send(dataCrush);
 });
 
-// app.get('/:id', async (req, res) => {
-//   const { id } = req.params;
-//   if () {
-//     res.status(200).send({
-
-// });
-//   } else {
-//     res.status(404).send({
-//       message: 'Crush não encontrado',
-//     });
-//   }
-// });
+app.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const dataCrush = await readCrush();
+  const filterIdData = dataCrush.find((element) => element.id === Number(id));
+  if (!filterIdData) {
+    return res.status(404).send({
+      message: 'Crush não encontrado',
+    });
+  }
+  return res.status(200).send(filterIdData);
+});
 
 module.exports = app;
