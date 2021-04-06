172d5c369caadca4665f21940e24c44a8b1112b0
Breno Alves <brenoalv3ss@gmail.com>
2021-04-05 22:25:28 -0300

    Requisito 3

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
-const NOT_FOUND = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -26,8 +26,28 @@ app.get('/crush/:id', (request, response) => {
   if (filter) {
     response.status(SUCCESS).json(filter);
   } else {
-    response.status(NOT_FOUND).json({ message: 'Crush não encontrado' });
+    response.status(404).json({ message: 'Crush não encontrado' });
   }
+});
+
+app.post('/', (req, res) => {
+  const { email, password } = req.body;
+  const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  
+  if (!regex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+  }
+
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  const token = crypto.createHash('md5').update(email + password).digest('hex').slice(0, 16);
+  return res.status(SUCCESS).json({ token });
 });
 
 app.listen(PORT, () => { console.log('Online'); });
