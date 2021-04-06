770d3c7eb9acadc98ad8a589e596a1ecb1576ee4
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2021-04-06 18:43:46 -0300

    consertando mensagem requisito 3

--- middlewares/passwordMiddleware.js
+++ middlewares/passwordMiddleware.js
@@ -2,7 +2,7 @@ const passwordMiddleware = (req, res, next) => {
   const { password } = req.body;
   if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   if (password.length < 6) {
-    return res.status(400).json({ message: 'A "senha" ter pelo menos 6 caracteres' });
+    return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
 }
   next();
 };
