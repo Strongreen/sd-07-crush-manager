4806c775dedf237f8d0e22b27484d5447b5dd2ec
alvarovascon <alvarovascon2@gmail.com>
2021-04-06 17:11:54 -0300

    req 7

--- routes/crush.js
+++ routes/crush.js
@@ -12,6 +12,20 @@ const {
   dateRateMW,
 } = require('../middlewares');
 
+route.get('/search', auth, rescue(async (req, res, next) => {
+  const { q } = req.query;
+
+  if (!q) {
+    next();
+  }
+  const content = await fs.promises.readFile(`${__dirname}/../crush.json`, 'utf-8');
+  const crushesArray = JSON.parse(content);
+  
+  const filteredCrushes = crushesArray.filter((crush) => crush.name.includes(q));
+
+  res.status(200).json(filteredCrushes);
+}));
+
 route.get('/', rescue(async (_req, res) => {
   try {
     const content = await fs.promises.readFile(`${__dirname}/../crush.json`);
