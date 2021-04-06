15dc66c215328ee14e7daa0a510d4e25c96a4731
Anderson Santana <anderslash@gmail.com>
2021-04-06 18:02:52 -0300

    Req 01 - endpoint All Crushes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const crushFile = './crush.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +13,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+app.get('/crush', (req, res) => {
+  const crushes = JSON.parse(fs.readFileSync(crushFile));
+  if (!crushes.length) res.status(200).send([]);
+  res.status(200).send(crushes);
 });
 
 app.listen(PORT, () => { console.log('Online'); });
