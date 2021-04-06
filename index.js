c16c1e2210d23500eb7a54d70efadd314c7be8b0
nevesfreire <nando.neves@gmail.com>
2021-04-05 21:33:37 -0300

    teste

--- index.js
+++ index.js
@@ -57,14 +57,45 @@ app.post('/login', (req, res) => {
   });
 });
 
+function validateToken(authorization, res) {
+  if (!authorization) { res.status(401).send({ message: 'Token não encontrado' }); }
+  if (authorization.length !== 16) { res.status(401).send({ message: 'Token inválido' }); }
+}
+function validateName(name, res) {
+  if (name.length < 3) {
+    res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!name) { res.status(400).send({ message: 'O campo "name" é obrigatório' }); }
+}
+function validateAge(age, res) {
+  if (!age) { res.status(400).send({ message: 'O campo "age" é obrigatório' }); }
+  if (age < 18) { res.status(400).send({ message: 'O crush deve ser maior de idade' }); }
+}
+function validateRate(rate, res) {
+  if (rate < 1 || rate > 5 || rate !== Math.floor(rate)) {
+    res.status(400).send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+}
+
 app.post('/crush', (req, res) => {
   const { authorization } = req.headers;
-  if (!authorization) {
-    res.status(401).send({ message: 'Token não encontrado' });
+  const { name, age, date } = req.body;
+  const { datedAt, rate } = date;
+  const regex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  const regexValidation = regex.test(datedAt);
+  validateToken(authorization, res); validateName(name, res);
+  validateAge(age, res); validateRate(rate, res);
+  if (!regexValidation) {
+    res.status(400).send({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
   }
-  if (authorization.length !== 16) {
-    res.status(401).send({ message: 'Token inválido' });
+  if (!date || !rate || !datedAt) {
+    res.status(400).send({
+      message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
+    });
   }
+  res.status(201).send({
+    id: 1, name: 'Keanu Reeves', age: 56, date: { datedAt: '22/10/2019', rate: 5 },
+  });
 });
 
 app.listen(PORT, () => { console.log('Online'); });
