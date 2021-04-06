d2f1badf6da4764ad899e1038d38531fa891a457
moacyrrln <moacyrrln@gmail.com>
2021-04-05 21:10:35 -0300

    regex correção

--- dateMiddleware.js
+++ dateMiddleware.js
@@ -5,7 +5,7 @@ const dateMiddleware = (req, res, next) => {
       message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
     });
   }
-  const regex = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g;
+  const regex = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
   const dataValidation = regex.test(date.datedAt);
   if (!dataValidation) {
     return res.status(400).send({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
