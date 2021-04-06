ad876d90c753a0c2c2795aab5966617fb5ed5f52
Bruno <bruno_grb@hotmail.com>
2021-04-06 10:05:22 -0300

    iniciando desafio2

--- routes/crushes.js
+++ routes/crushes.js
@@ -1,36 +1,46 @@
 const express = require('express');
 const fs = require('fs');
-const data = require('../crush.json');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  const size = data.length;
-  if (size === 0) {
-    res.status(200).send([]);
-  }
+  const data = JSON.parse(fs.readFileSync(__dirname + '/../crush.json', 'utf8'));
+  console.log(data);
   res.status(200).send(data);
 });
 
 app.post('/', async (req, res) => {
+  const data = await fs.promises.readFile(__dirname + '/../crush.json');
   const size = data.length;
   data[size] = {
     name: req.body.name,
     age: req.body.name,
     id: size + 1,
-    date: {
-      datedAt: req.body.datedAt,
-      rate: req.body.rate,
-    },
+    date: { datedAt: req.body.datedAt, rate: req.body.rate },
   };
   try {
     await fs.promises.writeFile(`${__dirname}/../crush.json`, JSON.stringify(data));
     res.status(201).send({
       message: 'Salvo com sucesso!',
     });
-  } catch (error) {
-    throw new Error(error);
-  }
+  } catch (error) { throw new Error(error) }
+});
+
+app.put('/:id', async (req, res) => {
+  const data = await fs.promises.readFile(__dirname + '/../crush.json');
+  const size = data.length;
+  data[id - 1] = {
+    name: req.body.name,
+    age: req.body.name,
+    id: size + 1,
+    date: { datedAt: req.body.datedAt, rate: req.body.rate },
+  };
+  try {
+    await fs.promises.writeFile(`${__dirname}/../crush.json`, JSON.stringify(data));
+    res.status(201).send({
+      message: 'Salvo com sucesso!',
+    });
+  } catch (error) { throw new Error(error) }
 });
 
 module.exports = app;
