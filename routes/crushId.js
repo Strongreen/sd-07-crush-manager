c000f792ef3fb054eecffd704217d3692ea46285
alvarovascon <alvarovascon2@gmail.com>
2021-04-06 16:50:09 -0300

    req 6

--- routes/crushId.js
+++ routes/crushId.js
@@ -63,4 +63,28 @@ route.put(
   }),
 );
 
+route.delete(
+  '/:id',
+  auth,
+  rescue(async (req, res) => {
+    const { id } = req.params;
+
+    const content = await fs.promises.readFile(`${__dirname}/../crush.json`);
+    const crushesArray = JSON.parse(content);
+
+    const filteredCrushes = crushesArray.filter(
+      (crush) => crush.id !== Number(id),
+    );
+
+    await fs.promises.writeFile(
+      `${__dirname}/../crush.json`,
+      JSON.stringify(filteredCrushes),
+    );
+
+    return res.status(200).json({
+      message: 'Crush deletado com sucesso',
+    });
+  }),
+  );
+
 module.exports = route;
