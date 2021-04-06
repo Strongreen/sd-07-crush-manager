c3cee4976beb479014a4a3c7aec5ea17fbf9a3d6
Melissa Gomes <mel.ge171201@gmail.com>
2021-04-06 19:36:42 -0300

    subindo para o rufis ver o problema

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const data = require('./crush.json');
 const fs = require('fs').promises;
 
 const app = express();
@@ -21,9 +22,9 @@ app.listen(PORT, () => { console.log('Online'); });
   return rejexForEmail.test(email);
 };
 
-const passwordValidation = (password) => {
-  const rejexForPassword = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
-  return rejexForPassword.test(password);
+const dateValidation = (datedAt) => {
+  const regexForDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  return regexForDate.test(datedAt);
 };
 
 const authorizationEmail = (req, res, next) => {
@@ -38,6 +39,59 @@ const authorizationEmail = (req, res, next) => {
   if (!validEmail) {
     return res.status(400).json({
       message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+
+  next();
+};
+
+const validateAll = (crush) => {
+  const { name, age, date } = crush;
+
+  console.log(crush);
+  if (!name) {
+    throw new Error('O campo "name" é obrigatório');
+  }
+
+  if (name.length < 3) {
+    throw new Error('O "name" deve ter pelo menos 3 caracteres');
+  }
+
+  if (!age) {
+    throw new Error('O campo "age" é obrigatório');
+  }
+
+  if (age < 18) {
+    throw new Error('O crush deve ser maior de idade');
+  }
+
+  if (!date) {
+    throw new Error('O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios');
+  }
+
+  const { datedAt, rate } = date;
+  const validData = dateValidation(datedAt);
+  if (!validData) {
+    throw new Error('O campo "datedAt" deve ter o formato "dd/mm/aaaa"');
+  }
+
+  if (!Number.isInteger(rate) || rate < 1 || rate > 5) {
+    throw new Error('O campo "rate" deve ser um inteiro de 1 à 5');
+  }
+};
+
+const validationToken = (req, res, next) => {
+  const { authorization: token } = req.headers;
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Token não encontrado',
+    });
+  }
+
+  if (token.length !== 16) {
+    return res.status(401).json({
+      message: 'Token inválido',
     });
   }
 
@@ -70,8 +124,8 @@ app.post('/login', authorizationEmail, (req, res) => {
       message: 'O campo "password" é obrigatório',
     });
   }
-  passwordValidation(password);
-  if (!password || password.length < 6) {
+
+  if (password.length < 6) {
     return res.status(400).json({
       message: 'A "senha" deve ter pelo menos 6 caracteres',
     });
@@ -80,4 +134,27 @@ app.post('/login', authorizationEmail, (req, res) => {
    return res.status(SUCCESS).json({
     token: '7mqaVRXJSp886CGr',
   });
+});
+
+app.post('/crush', validationToken, async (req, res) => {
+  const size = data.length;
+  
+  try {
+    validateAll(req.body);
+    const { name, age, date } = req.body;
+    const myObj = {
+    name,
+    age,
+    id: size + 1,
+    date,
+  };
+  
+  data[size] = myObj;
+    await fs.writeFile(`${__dirname}/crush.json`, JSON.stringify(data));
+    res.status(201).json(myObj);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 });
