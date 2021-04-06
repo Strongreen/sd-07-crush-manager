4d07f86d353cdf51e2a7ba0e5cb4aa78b1103609
Strongreen <strongreen@live.com>
2021-04-06 15:34:34 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const readFileMiddleware = require('./Middlewares/readFileMiddleware');
 
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
+const BAD = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', readFileMiddleware);
 
 app.listen(PORT, () => { console.log('Online'); });
