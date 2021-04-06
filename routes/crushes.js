fc6722c48ec7ec3887c2370d11d2b2fbdfd8726e
Bruno <bruno_grb@hotmail.com>
2021-04-06 13:57:51 -0300

    desafio 2 pronto

--- routes/crushes.js
+++ routes/crushes.js
@@ -4,43 +4,24 @@ const fs = require('fs');
 const app = express();
 
 app.get('/', (req, res) => {
-  const data = JSON.parse(fs.readFileSync(__dirname + '/../crush.json', 'utf8'));
-  console.log(data);
+  const data = JSON.parse(fs.readFileSync(`${__dirname}/../crush.json`, 'utf8'));
   res.status(200).send(data);
 });
 
-app.post('/', async (req, res) => {
-  const data = await fs.promises.readFile(__dirname + '/../crush.json');
-  const size = data.length;
-  data[size] = {
-    name: req.body.name,
-    age: req.body.name,
-    id: size + 1,
-    date: { datedAt: req.body.datedAt, rate: req.body.rate },
-  };
+app.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const data = JSON.parse(fs.readFileSync(`${__dirname}/../crush.json`, 'utf8'));
   try {
-    await fs.promises.writeFile(`${__dirname}/../crush.json`, JSON.stringify(data));
-    res.status(201).send({
-      message: 'Salvo com sucesso!',
+    const crushID = data[id - 1];
+    if (!crushID) {
+      throw new Error('Crush não encontrado');
+    }
+    res.status(200).send(crushID);
+  } catch (error) {
+    res.status(404).send({
+      message: error.message,
     });
-  } catch (error) { throw new Error(error) }
-});
-
-app.put('/:id', async (req, res) => {
-  const data = await fs.promises.readFile(__dirname + '/../crush.json');
-  const size = data.length;
-  data[id - 1] = {
-    name: req.body.name,
-    age: req.body.name,
-    id: size + 1,
-    date: { datedAt: req.body.datedAt, rate: req.body.rate },
-  };
-  try {
-    await fs.promises.writeFile(`${__dirname}/../crush.json`, JSON.stringify(data));
-    res.status(201).send({
-      message: 'Salvo com sucesso!',
-    });
-  } catch (error) { throw new Error(error) }
+  }
 });
 
 module.exports = app;
