9b2c85fd56d34dd7551086f00e79036dc2571065
Lucas <lafdesouza2002@outlook.com>
2021-04-06 16:03:41 -0300

    Alguns desafios

--- middlewares/passwordLength.js
+++ middlewares/passwordLength.js
@@ -1,6 +1,8 @@
 const passwordLength = (req, res, next) => {
     const { password } = req.body;
-    if (password.length < 6) {
+    const passwordStr = password.toString();
+    const isValid = passwordStr.length > 6;
+    if (!isValid) {
         return res.status(400).send({
             message: 'A "senha" deve ter pelo menos 6 caracteres',
 
