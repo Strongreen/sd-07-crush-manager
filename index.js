0d95457c1f39e77828f06f678436bb06a76a3510
Renato Silva <renatosdev@gmail.com>
2021-04-06 15:25:01 -0300

    requisito1

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routerCrush = require('./routes/crush');
 
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
-const PORT = '3000';
+const PORT = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.listen(PORT, () => { console.log('Online'); });
+app.use('/crush', routerCrush);
+
+app.listen(PORT, () => {
+  console.log('Online');
+});
---
+++ routes/crush.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const app = express();
+
+const readCrush = async () => {
+  return JSON.parse(await fs.readFile('./crush.json', 'utf8'));
+};
+
+app.get('/', async (req, res) => {
+  const result = await readCrush();
+  return res.status(200).send(result);
+});
+
+// app.get('/:id', async (req, res) => {
+//   const { id } = req.params;
+//   if () {
+//     res.status(200).send({
+
+// });
+//   } else {
+//     res.status(404).send({
+//       message: 'Crush não encontrado',
+//     });
+//   }
+// });
+
+module.exports = app;
