1e63247524e08758dad3392db021451b75a7d191
vitorAmorims <vtamorims@gmail.com>
2021-04-07 20:14:49 -0300

    testes

--- middlewares/authToken.js
+++ middlewares/authToken.js
@@ -1,5 +1,6 @@
 const authToken = (req, res, next) => {
     const token = req.headers.authorization;
+    // console.log(token)
     if (!token) return res.status(401).json({ message: 'Token não encontrado' });
     if (token.length !== 16) return res.status(401).json({ message: 'Token inválido' });
     next();
