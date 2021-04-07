2b469610302f24f3bac2cb748c84c0fa48c2a12f
Carol Bezerra <carol.arbe@gmail.com>
2021-04-06 22:22:37 -0300

    Do 1 ao 6 no local

--- crush.js
+++ crush.js
@@ -9,9 +9,7 @@ const crushJson = './crush.json';
 const crushFile = () => fs.promises.readFile(crushJson, 'utf8');
 // desse jeito porque vai ser chamado várias outras vezes (Plantão Rufino);
 
-app.get('/', async (_req, res) => {
-  res.status(200).json(JSON.parse(await crushFile()));
-}); // requisito 1
+app.get('/', async (_req, res) => res.status(200).json(JSON.parse(await crushFile()))); // requisito 1
 
 async function findAsyncId(id) {
   const content = await crushFile();
@@ -23,7 +21,7 @@ app.get('/:id', async (req, res) => {
   const isId = await findAsyncId(id);
   if (!isId) return res.status(404).send({ message: 'Crush não encontrado' });
   
-  res.status(200).send(isId);
+  return res.status(200).send(isId);
 }); // requisito 2
 
 function isValidName(name) {
@@ -77,32 +75,12 @@ function isValidAuthorization(auth) {
   
   if (!auth) {
     response = 'Token não encontrado';
-  } else if (auth.length < 16) {
+  } else if (auth.length !== 16) {
     response = 'Token inválido';
   }
 
   return response;
 }
-
-app.post('/', async (req, res) => {
-  const { authorization } = req.headers;
-  const isAuth = isValidAuthorization(authorization);
-  if (isAuth) return res.status(401).send({ message: isAuth });
-
-  const { name, age, date } = req.body;
-  const isName = isValidName(name);
-  if (isName) return res.status(400).send({ message: isName });
-  const isAge = isValidAge(age);
-  if (isAge) return res.status(400).json({ message: isAge });
-  const isDate = isValidDate(date);
-  if (isDate) return res.status(400).send({ message: isDate });
-  
-  const crushes = await crushFile();
-  const newCrush = { id: JSON.parse(crushes).length + 1, name, age, date };
-  const addCrush = [...crushes, newCrush];
-  await fs.promises.writeFile(crushJson, JSON.stringify(addCrush));
-  return res.status(201).json(newCrush);
-});
 
 function completeAuth(objParams) {
   const { authorization, res, name, age, date } = objParams;
@@ -115,6 +93,22 @@ function completeAuth(objParams) {
   const isDate = isValidDate(date);
   if (isDate) return res.status(400).send({ message: isDate });
 }
+
+app.post('/', async (req, res) => {
+  const { authorization } = req.headers;
+  const isAuth = isValidAuthorization(authorization);
+  if (isAuth) return res.status(401).send({ message: isAuth });
+
+  const { name, age, date } = req.body;
+  const params = { authorization, res, name, age, date };
+  completeAuth(params);
+  
+  const crushes = await crushFile();
+  const newCrush = { id: JSON.parse(crushes).length + 1, name, age, date };
+  const addCrush = [...crushes, newCrush];
+  await fs.promises.writeFile(crushJson, JSON.stringify(addCrush));
+  return res.status(201).json(newCrush);
+});
 
 app.put('/:id', async (req, res) => {
   const { authorization } = req.headers;
@@ -131,8 +125,17 @@ app.put('/:id', async (req, res) => {
   }
 });
 
-app.use((err, _req, res, _next) => {
-  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
+app.delete('/:id', async (req, res) => {
+  const { authorization } = req.headers;
+  const isAuth = isValidAuthorization(authorization);
+  if (isAuth) return res.status(401).send({ message: isAuth });
+
+  const { id } = req.params;
+  const crushes = await crushFile();
+  const newArrayCrushes = JSON.parse(crushes).filter((item) => Number(item.id) !== Number(id));
+
+  await fs.promises.writeFile(crushJson, JSON.stringify(newArrayCrushes));
+  return res.status(200).json({ message: 'Crush deletado com sucesso' });
 });
 
 module.exports = app;
