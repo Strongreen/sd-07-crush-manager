c8335f9b671b72d8db1545a108a1177f75710086
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-04-07 13:54:57 -0300

    Arrumento um pouco a bagunça :rocket:

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const helpers = require('./helper/validateCreate');
 const desafio1 = require('./desafio1');
-// const desafio2 = require('./desafio2');
 const desafio3 = require('./desafio3');
 const desafio4 = require('./desafio4');
 const desafio5 = require('./desafio5');
 const desafio6 = require('./desafio6');
-// const desafio7 = require('./desafio7');
 const middlewares = require('./middlewares/validateToken');
 
 const app = express();
@@ -29,7 +27,7 @@ app.get('/crush/search', middlewares.validateToken, async (req, res) => {
   const crush = JSON.parse(await fs.readFile('./crush.json', 'utf8'));
   try {
     const filterName = helpers.filterByName(crush, req.query.q);
-    res.status(200).json(filterName);
+    res.status(SUCCESS).json(filterName);
   } catch (error) {
     return res.status(400).json({
       message: error.message,
@@ -44,7 +42,7 @@ app.get('/crush/:id', async (req, res) => {
     const crush = JSON.parse(await fs.readFile('./crush.json'));
     const filter = crush.filter((person) => person.id === parseInt(id, 10));
     if (filter.length > 0) {
-      res.status(200).json(filter[0]);
+      res.status(SUCCESS).json(filter[0]);
     } else {
       throw new Error();
     }
