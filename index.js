501a3dc943b09141fa2555945fd499071c20b243
Fabio Camargos <fabiocmmcm@gmail.com>
2021-04-08 15:24:42 -0300

    iniciando projeto Crush

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
-
 const SUCCESS = 200;
-const PORT = '3000';
+const PORT = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
