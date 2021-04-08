d3e697f23ce078c9998bb6ba18ab491009345760
Macarena Rodriguez <macarenar697@gmail.com>
2021-04-08 10:24:09 -0300

    refatoracao3

--- routes/crushAdd.js
+++ routes/crushAdd.js
@@ -1,12 +1,32 @@
 const validToken = require('../valid/validToken');
+const file = require('./crushFile');
 
+const crushData = 'crush.json';
 const { validTokenFunction } = validToken;
+
+async function crushAdd(body, response) {
+  try {
+    const { name, age, date } = body;
+    const showFile = await file.readPromise(crushData);
+    const newCrush = { id: 1, name, age, date };
+    showFile.push(newCrush);
+    console.log(showFile);
+    return response.status(201).send({
+      id: newCrush.id,
+      name: newCrush.name,
+      age: newCrush.age,
+      date: newCrush.date,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+}
 
 function crushAddFunction(request, response) {
   const { authorization } = request.headers;
-  console.log(authorization);
+  const { body } = request;
   validTokenFunction(authorization, response);
-  console.log('oioioi');
+  crushAdd(body, response);
 }
 
 module.exports = { crushAddFunction };
