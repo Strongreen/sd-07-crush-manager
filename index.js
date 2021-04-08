3b92bf45160535c86a602e9a3139890d152c1352
Flavio Oliveira <flavsoliver@gmail.com>
2021-04-08 15:22:14 -0300

    refatoração - mudança de estrutura de rotas

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const dataCrush = require('./crush.json');
+
+const routes = require('./routes');
+// const middlewares = require('./middlewares');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,32 +10,13 @@ app.use(bodyParser.json());
 const SUCCESS = 200;
 const PORT = '3000';
 
-const crushList = 'crush.json';
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.get('/crush', async (_req, res) => {
-  const crush = await fs.promises.readFile(crushList);
-  if (crush) {
-    return res.status(200).json(JSON.parse(crush));
-  }
-  return res.status(200).json([]);
-});
-
-app.get('/crush/:id', (req, res) => {
-  const { id: reqId } = req.params;
-  const crushId = dataCrush.find(({ id }) => id === Number(reqId));
-  if (crushId) {
-    return res.status(200).json(crushId);
-  }
-  return res.status(404).json({
-    message: 'Crush não encontrado',
-  });
-});
-
-app.get('/crush', (_req, res) => res.status(200).json(dataCrush));
+app.use(express.json());
+app.use(routes.getAllCrush);
+app.use(routes.getCrushId);
 
 app.listen(PORT, () => { console.log('Online'); });
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const getAllCrush = require('./getAllCrush');
+const getCrushId = require('./getCrushId');
+
+module.exports = {
+  getAllCrush,
+  getCrushId,
+};
