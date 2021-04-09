5c72bd417f7ee7a9f7822676f1abafae1af897de
mateus <mateuslaalmeida@gmail.com>
2021-04-09 13:41:18 -0300

    Alterado retorno do desafio 3

--- routes/login.js
+++ routes/login.js
@@ -20,7 +20,7 @@ app.post('/', (req, res) => {
   } if (!password) {
     return res.status(400).send({ message: 'O campo "password" é obrigatório' });
   } if (password.length < 6) {
-    return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    return res.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   }
   return res.status(200).send({ token });
 });
