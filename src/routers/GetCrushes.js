f11b77251cf1db00ff989a35ed0833add87c3dfb
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-04-08 22:38:26 -0300

    Exercise 1 - done-4

--- src/routers/GetCrushes.js
+++ src/routers/GetCrushes.js
@@ -3,15 +3,11 @@ const express = require('express');
 
 const Crush = express.Router();
 
-const getCrushes = async () => {
-  const result = await fs.readFile(`${__dirname}/../data/crush.json`);
-  return result;
-};
-
 Crush.get('/', async (_req, res) => {
   const noCrush = [];
+  const result = await fs.readFile(`${__dirname}/../data/crush.json`);
+  const crush = result;
   try {
-    const crush = await getCrushes();
     if (crush.length !== 0) return res.status(200).send(crush);
     return res.status(200).send(noCrush);
   } catch (error) {
