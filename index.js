141b86f880fb658a6d1bc857b80246c4aecb486b
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-04-09 02:04:13 -0300

    Exercise 2 - done

--- index.js
+++ index.js
@@ -13,12 +13,27 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
+// Requisito 1
 app.get('/crush', async (req, res) => {
   try {
     const response = await fs.readFile(path.join(__dirname, './crush.json'));
     const data = await JSON.parse(response);
     if (response.length === 0) return res.status(200).send([]);
     res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Requisito 2
+app.get('/crush/:id', async (req, res) => {
+  const { id } = req.params;
+  const response = await fs.readFile(path.join(__dirname, './crush.json'));
+  const data = JSON.parse(response);
+  const data2 = data.filter((element) => element.id === +id)[0];
+  try {
+    if (!data2) return res.status(404).send({ message: 'Crush não encontrado' });
+    res.status(200).send(data2);
   } catch (error) {
     console.log(error);
   }
