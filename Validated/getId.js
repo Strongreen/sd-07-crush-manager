4440b9aeaef8d636cf64c7d813ca1fac09ca3a20
Strongreen <strongreen@live.com>
2021-04-09 09:17:04 -0300

    correçao do lint

--- Validated/getId.js
+++ Validated/getId.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function getId() {
   const path = `${__dirname}/../crush.json`;
-  const crushes = JSON.parse( fs.readFileSync(path, 'utf8'));
+  const crushes = JSON.parse(fs.readFileSync(path, 'utf8'));
   return { crushes, id: Number(crushes.length) };
 }
 
