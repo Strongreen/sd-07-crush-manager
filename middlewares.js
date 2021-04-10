bbf58a552c3b549b6c7d5ae8b478ef8a355fa44f
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-04-10 02:43:14 -0300

    implementado funções booleanas para validação no endpoint POST crush.

--- middlewares.js
+++ middlewares.js
@@ -29,3 +29,35 @@ exports.validateTokenMiddleware = (req, res, next) => {
 
 exports.errorMiddleware = (err, _req, res, _next) => 
      res.status(err.status).send({ message: err.message });
+
+/*
+{
+  "message": "O campo \"name\" é obrigatório"
+}
+{
+  "message": "O \"name\" deve ter pelo menos 3 caracteres"
+}
+
+{
+  "message": "O campo \"age\" é obrigatório"
+}
+{
+  "message": "O crush deve ser maior de idade"
+}
+
+{
+  "message": "O campo \"datedAt\" deve ter o formato \"dd/mm/aaaa\""
+}
+
+{
+  "message": "O campo \"rate\" deve ser um inteiro de 1 à 5"
+}
+
+O campo date é obrigatório e nenhuma das chaves citadas anteriormente podem ser vazias.
+
+Caso o campo não seja informa, esteja vazio ou então alguma de suas chaves não tenham sido informadas retorne status 400, com o seguinte corpo:
+
+{
+  "message": "O campo \"date\" é obrigatório e \"datedAt\" e \"rate\" não podem ser vazios"
+}
+*/
--- services/crushService.js
+++ services/crushService.js
@@ -28,6 +28,26 @@ async function deleteCrushById(id) {
   }
 }
 
+function isValidName(name) {
+  if (name.length <= 3) return false;
+  return true;
+}
+
+function isOverEighteen(age) {
+  if (age < 18) return false;
+  return true;
+}
+
+function isValidDate(date) {
+  const regex = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+  if (!date.match(regex)) return false;
+  return true;
+}
+
+function isRateInRange(rate) {
+  if (rate >= 1 && rate <= 5) return true;
+  return false;
+}
 /* 
 function setCrushs(newCrush) {
     const crushs = await getAllCrushs();
@@ -48,4 +68,8 @@ module.exports = {
   getAllCrushs,
   getCrushById,
   deleteCrushById,
+  isValidName,
+  isOverEighteen,
+  isValidDate,
+  isRateInRange,
 };
