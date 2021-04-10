f066ace9b85437b948a4dc75cfc15a1db82bf29a
PedroMarqDev <PedroMarqDev@gmail.com>
2021-04-10 18:51:37 -0300

    ex 1 e 2

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const crushData = require("./crush.json");
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
-const PORT = '3000';
+const PORT = "3000";
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
+app.get("/", (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.listen(PORT, () => { console.log('Online'); });
+app.get("/crush", (req, res) => {
+  if (crushData.length === 0) res.status(200).send([]);
+  res.status(200).send(crushData);
+});
+
+app.get("/crush/:id", (req, res) => {
+  const crush = crushData.find((element) => req.params.id == element.id);
+  crush
+    ? res.status(200).send(crush)
+    : res.status(404).send({
+        message: "Crush não encontrado",
+      });
+});
+
+app.listen(PORT, () => {
+  console.log("Online");
+});
