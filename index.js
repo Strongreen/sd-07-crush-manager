49da62da6e4b70f0a1914ccf0d0ae5021abe532b
Brune Miguel <bstfreire@gmail.com>
2021-04-10 01:35:31 -0300

    Corrigindo eslint

--- index.js
+++ index.js
@@ -13,27 +13,25 @@ const loginControllers = require('./Controllers/indexLogin');
 const app = express();
 const SUCCESS = 200;
 const PORT = '3000';
-const CRUSH = '/crush';
 const CRUSH_ID = '/crush/:id';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
-
 app.use(bodyParser.json());
-app.get(CRUSH, crushControllers.getCrush);
-app.post(CRUSH, validatedToken, validatedNameAge, validatedDate, crushControllers.addCrush);
+app.get('/crush', crushControllers.getCrush);
+app.post('/crush', validatedToken, validatedNameAge, validatedDate, crushControllers.addCrush);
 app.get(CRUSH_ID, crushControllers.getCrushById);
 app.post('/login', loginControllers.getLogin);
 app.put(
-  '/crush/:id',
+  CRUSH_ID,
   validatedToken,
   validatedNameAge,
   validatedDate,
   crushControllers.updateCrush,
 );
 app.delete(
-  '/crush/:id',
+  CRUSH_ID,
   validatedToken,
   validatedNameAge,
   validatedDate,
