b988fcb8e27b95b6e1882b77617cd568a6b1116c
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-04-09 22:11:43 -0300

    Requisito 4

--- index.js
+++ index.js
@@ -8,21 +8,22 @@ app.use(bodyParser.json());
 
 const SUCCESS = 200;
 const PORT = '3000';
-// const crushTest = {
+
+// const testChush = {
 //   name: 'Zendaya Maree',
 //   age: 24,
 //   id: 5,
 //   date: { rate: 5, datedAt: '25/09/2020' },
 // };
 
-// a ordem importa porque o primeiro que está chegando e o segundo oq esta saindo
+// a ordem importa porque o primeiro que está chegando e o segundo oq esta saindo (?)
 app.get('/crush', (req, res) => {
   fs.readFile('./crush.json', 'utf8', (err, data) => {
     if (err) {
       console.error(`Não foi possível ler o arquivo ${data}\n Erro: ${err}`);
       process.exit(1);
     }
-    console.log(`Conteúdo do arquivo: ${data}`);
+    // console.log(`Conteúdo do arquivo: ${data}`);
     res.status(SUCCESS).json(JSON.parse(data));
   });
 });
@@ -42,7 +43,7 @@ app.get('/crush/:id', (req, res) => {
         message: 'Crush não encontrado',
       });
     } else {
-      console.log(`Conteúdo do arquivo id: ${idData}`);
+      // console.log(`Conteúdo do arquivo id: ${idData}`);
       res.status(SUCCESS).json(idData);
     }
   });
@@ -54,26 +55,24 @@ app.post('/login', (req, res) => {
     res.status(400).send({ message: 'O campo "email" é obrigatório' });
   } else if (!password) {
     res.status(400).send({ message: 'O campo "password" é obrigatório' });
-  } else if (email === 'eu não sou um email') {
-    // consertar
-    res
-      .status(400)
-      .send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  } else if (email === 'eu não sou um email') { // consertar
+    res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
   } else if (password.length < 6) {
-    res
-      .status(400)
-      .send({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+    res.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   } else {
-    console.log('salvo');
+    console.log('Login salvo');
     res.status(SUCCESS).json({ token: '7mqaVRXJSp886CGr' });
   }
 });
 
 const existenceError = (req, res, objName) => {
   if (!req) {
-    return res
-      .status(400)
-      .send({ message: `O campo ${objName} é obrigatório` });
+    if (objName === 'name') {
+    return res.status(400).send({ message: 'O campo "name" é obrigatório' });
+    }
+    if (objName === 'age') {
+      return res.status(400).send({ message: 'O campo "age" é obrigatório' });
+      }
   }
 };
 
@@ -81,31 +80,41 @@ const tokenValidation = (req, res) => {
   if (!req) {
     return res.status(401).send({ message: 'Token não encontrado' });
   }
-  if (req === 99999999) {
-    // consertar
+  // console.log(`token validation ${req}`);
+  if (req.length < 16) {
     return res.status(401).send({ message: 'Token inválido' });
   }
   return console.log('Token ok');
 };
 
-const dateFormValidation = (req, res) => {
+const dateValidation2 = (req, res) => {
+  console.log('entrei date validation 2');
   if (req.datedAt === '42-20-3333') { // consertar
     return res.status(400).send({ 
       message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
     });
   }
+  if (req.rate > 5 || req.rate < 1) {
+    return res
+      .status(400)
+      .send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  return console.log('Date form ok');
 };
 
 const dateValidation = (req, res) => {
-  dateFormValidation(req, res);
-  if (!req.datedAt || !req.rate) {
+  console.log('entrei date validation');
+  if (!req) {
     return res.status(400).send({
       message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
     });
   }
-  if (req.rate > 5 || req.rate < 1) {
-    return res.status(400).send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-   }
+  dateValidation2(req, res);
+  if (!req.datedAt || !req.rate) {
+    return res.status(400).send({
+      message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
+    });
+  }   
    return console.log('Date ok');
 };
 
@@ -120,13 +129,13 @@ app.post('/crush', (req, res) => {
     res
       .status(400)
       .send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-  } else if (age.length < 18) {
+  } else if (age < 18) {
     res.status(400).send({ message: 'O crush deve ser maior de idade' });
   } else {
-    console.log('salvo');
+    console.log('Tudo ok, salvo');
     res
       .status(201)
-      .json({ name, age, date: { datedAt: date.datedAt, rate: date.rate } });
+      .json({ name, age, id: 5, date: { datedAt: date.datedAt, rate: date.rate } }); // consertar id
   }
 });
 
