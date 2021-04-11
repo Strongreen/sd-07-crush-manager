d0a3793c5adce1b5ebb8c350ae1a864febe0c0aa
Camila Valentim <camilaifpb@hotmail.com>
2021-04-11 15:33:38 -0300

    agora vai

--- routes/login.js
+++ routes/login.js
@@ -1,34 +1,32 @@
 const express = require('express');
 
-const router = express.Router();
-
+const app = express();
 const crypto = require('crypto');
 
 function validationEmail(email) {
-    // Referencia Luise rios
-    const emailVerify = /\S+@\S+\.\S+/;
-    return emailVerify.test(email);
+  const emailval = /\S+@\S+\.\S+/;
+  return emailval.test(email);
 }
- function validationPassword(password) {
-    return password.toString().length < 6;
- }
 
-router.post('/', (req, res) => {
-    const { email, password } = req.body;
-    if (!email) {
-      return res.status(400).send({ message: 'O campo "email" é obrigatório' }); 
-    } if (!password) {
-    return res.status(400).send({ 
-    message: 'O campo "password" é obrigatório', 
-    }); 
+function validationPassword(password) {
+  return password.toString().length < 6;
 }
-    if (validationPassword(password)) {
-        return res.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' }); 
-    } if (!validationEmail(email)) {
-        return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' }); 
-    }
-    const tokenvalidation = crypto.randomBytes(8).toString('hex');
-    return res.status(200).send({ tokenvalidation });
+
+app.post('/', (req, res) => {
+  const { email, password } = req.body;
+  if (!email) return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  if (!password) return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  if (validationPassword(password)) {
+ return res.status(400).send({
+       message: 'A "senha" deve ter pelo menos 6 caracteres' }); 
+}
+  if (!validationEmail(email)) {
+ return res.status(400).send({ 
+      message: 'O "email" deve ter o formato "email@email.com"' }); 
+}
+
+  const token = crypto.randomBytes(8).toString('hex');
+  return res.status(200).send({ token });
 });
 
-module.exports = router;
+module.exports = app;
