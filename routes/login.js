b144096fe628403d4337efb06a824b577dab0480
Camila Valentim <camilaifpb@hotmail.com>
2021-04-11 15:41:50 -0300

    requisito 3

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const app = express();
+const router = express.Router();
 const crypto = require('crypto');
 
 function validationEmail(email) {
+    // Referencia Luise Rios
   const emailval = /\S+@\S+\.\S+/;
   return emailval.test(email);
 }
@@ -12,21 +13,21 @@ function validationPassword(password) {
   return password.toString().length < 6;
 }
 
-app.post('/', (req, res) => {
+router.post('/', (req, res) => {
   const { email, password } = req.body;
-  if (!email) return res.status(400).send({ message: 'O campo "email" é obrigatório' });
-  if (!password) return res.status(400).send({ message: 'O campo "password" é obrigatório' });
-  if (validationPassword(password)) {
- return res.status(400).send({
-       message: 'A "senha" deve ter pelo menos 6 caracteres' }); 
-}
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   if (!validationEmail(email)) {
- return res.status(400).send({ 
-      message: 'O "email" deve ter o formato "email@email.com"' }); 
+    return res.status(400).json({ 
+         message: 'O "email" deve ter o formato "email@email.com"' }); 
+   }
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (validationPassword(password)) {
+ return res.status(400).json({
+       message: 'A "senha" deve ter pelo menos 6 caracteres' }); 
 }
 
   const token = crypto.randomBytes(8).toString('hex');
   return res.status(200).send({ token });
 });
 
-module.exports = app;
+module.exports = router;
