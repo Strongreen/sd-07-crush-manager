5ef104311efcf5e81c3a352167f1c15141e11944
Camila Valentim <camilaifpb@hotmail.com>
2021-04-11 11:01:45 -0300

    requisito 2

--- routes/crush.js
+++ routes/crush.js
@@ -14,4 +14,14 @@ router.get('/', async (req, res) => {
   res.status(200).send(result);
 });
 
+router.get('/:id', async (req, res) => {
+  const result = await readCrushFile();
+  const { id } = req.params;
+  const filterCrush = result.find((crush) => crush.id === id);
+  if (!filterCrush) {
+    res.status(404).json({ message: 'Crush não encontrado' });
+  } 
+  return res.status(200).json(filterCrush);
+});
+
 module.exports = router;
