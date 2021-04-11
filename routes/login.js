571880a14fb95a77ce8309c5d97a11a37c6ec9d8
Camila Valentim <camilaifpb@hotmail.com>
2021-04-11 14:30:38 -0300

    requisito 3

--- routes/login.js
+++ routes/login.js
@@ -9,19 +9,19 @@ function token() {
 
 router.post('/', async (req, res, next) => {
     const { email, password } = req.body;
-    const validator = /\S+@\S+\.\S+/.test(email);
-    if (email) return res.status(400).json({ message: 'O campo "email" é obrigatório' }); 
+    const validator = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i.test(email);
+    if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' }); 
     if (!validator) {
  return res.status(400).json({ 
     message: 'O "email" deve ter o formato "email@email.com"', 
     }); 
 }
     if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' }); 
-    if (password.length < 6) {
+    if (!password.length <= 6) {
         return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' }); 
     }
-    // Referencia Emanuelle Brasil
-    res.status(200).json({ token: token() });
+    const tokenValid = token();
+    res.status(200).json({ tokenValid });
 
     next();
 });
