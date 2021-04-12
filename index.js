3d8e698a134912c374f14ceda722d24d6700232b
Erling <erlingjunior@gmail.com>
2021-04-12 09:47:13 -0300

    comecando req 4

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const { stringify } = require('querystring');
+const dataArr = require('./crush.json');
 
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
+const POST_SUCCESS = 201;
 const FAIL = 404;
 /* const FAIL_SERVER = 500; */
 const PORT = '3000';
@@ -31,10 +34,30 @@ app.get('/crush/:id', (req, res) => {
   const data2 = JSON.parse(fs.readFileSync('./crush.json', 'utf8'));
   console.log(data2);
   const { id } = req.params;
-  const crushId = parseInt(id, data2.length);
+  const crushId = parseInt(id, 10);
   const crushFind = data2.find((crush) => crush.id === crushId);
   if (crushFind) res.status(SUCCESS).send(crushFind);
     return res.status(FAIL).send({ message: 'Crush não encontrado' });
+  });
+
+  app.post('/crush', async (req, res) => {
+    const crushLength = dataArr.length;
+    dataArr[crushLength] = {
+      name: req.body.name,
+      age: req.body.age,
+      id: crushLength + 1,
+      date: `${req.body.date.datedAt}, ${req.body.date.rate}`,
+      
+    };
+
+    try {
+      await fs.promises.writeFile(`${__dirname}/./crush.json`, JSON.stringify(dataArr));
+      res.status(POST_SUCCESS).send({
+        message: 'Salvou essa bagaça',
+      });
+    } catch (error) {
+      res.status(500).send({ message: 'ta zuado o role' });
+    }
   });
 
 app.listen(PORT, () => { console.log('O Pai ta ON na Porta 3000'); });
