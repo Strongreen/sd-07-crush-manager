84dc9824fb2300aa69a9d9e20a09f9b4f331c91e
BrunoNogueiraBruno <nugobruno@gmail.com>
2021-04-11 22:02:19 -0300

    Requisito 2

--- routes/Crush.js
+++ routes/Crush.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const fs = require('fs');
 
-const app = express();
+const router = express.Router();
 const SUCCESS = 200;
+const FAILURE = 404;
 
 // app.get e fs.promises fonte: https://github.com/tryber/sd-07-crush-manager/blob/alexandrefaustino-sd-07-crush-manager/routes/crush.js
 
-async function getAllCrushes() {
+async function readCrushes() {
   const data = await fs.promises.readFile(`${__dirname}/../crush.json`);
   return JSON.parse(data.toString('utf-8'));
 }
 
-app.get('/', async (_request, response) => {
-  response.status(SUCCESS).send(await getAllCrushes());
+router.get('/', async (_req, res) => {
+  res.status(SUCCESS).send(await readCrushes());
 });
 
-module.exports = app;
+router.get('/:id', async (req, res) => {
+  const notFound = { message: 'Crush não encontrado' };
+  const { id } = req.params;
+  const data = await readCrushes();
+  const filtered = data.find((crush) => crush.id === Number(id));
+  if (filtered) {
+    res.status(SUCCESS).json(filtered);
+  } else {
+    res.status(FAILURE).json(notFound);
+  }
+});
+
+module.exports = router;
