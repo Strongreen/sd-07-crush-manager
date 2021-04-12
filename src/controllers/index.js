d34f610de70941def9c0b97481d4e2675014caf3
muriloPereira006 <muriloedu006@gmail.com>
2021-04-11 22:00:39 -0300

    error ECONNREFUSED mas no postman deu certo

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
-const data = require('../../crush.json');
 
 const SUCCESS = 200;
 const FAIL = 500;
 
 const getCrushes = async (req, res) => {
   try {
-    const resulte = await fs.promises.readFile(data);
-    return res.status(SUCCESS).send(resulte);
+    const result = await fs.promises.readFile(`${__dirname}/../../crush.json`, 'utf-8');
+    return res.status(SUCCESS).send(JSON.parse(result));
   } catch (error) {
     console.error(error);
     return res.status(FAIL).send({ menssage: error.menssage });
