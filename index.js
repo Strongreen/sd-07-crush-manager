bf393dc57c9bdbf7754fd8e363db9546a7698ad6
Erling <erlingjunior@gmail.com>
2021-04-12 09:52:26 -0300

    ajustando erros

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { stringify } = require('querystring');
 const dataArr = require('./crush.json');
 
 const app = express();
