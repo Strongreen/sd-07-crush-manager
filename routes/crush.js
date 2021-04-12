64fe18bca647819786c62a606edeca8be6d155f9
vini-lima <vini20.henrique@gmail.com>
2021-04-12 15:27:30 -0300

    Req 1 Correção

--- routes/crush.js
+++ routes/crush.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const crush = require('../crush.json');
+const fs = require('fs').promises;
+const rescue = require('express-rescue');
 
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-  if (crush.length > 0) {
-    res.status(200).send(crush);
-  } else {
-    res.status(200).send({});
+router.get('/', rescue (async (_req, res) => {
+  try {
+    const responseCrush = await fs.readFile(`${__dirname}/../crush.json`);
+    res.status(200).send(responseCrush);
+  } catch (error) {
+    return error;
   }
-});
+}));
+
+router.post('/', rescue (async (_req, res) => {
+  try {
+    const responseCrush = await fs.readFile(`${__dirname}/../crush.json`);
+    res.status(200).send(responseCrush);
+  } catch (error) {
+    return error;
+  }
+}));
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
