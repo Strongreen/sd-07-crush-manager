c7f1b986c21e71b7a97e299828e57af72ac16b62
Anderson Santana <anderslash@gmail.com>
2021-04-12 01:26:16 -0300

    Adicionando delecao de crushes em DELETE/crush/id

--- routes/crush.js
+++ routes/crush.js
@@ -5,9 +5,13 @@ const auth = require('../middlewares/auth');
 const { crushValidation, dateValidation } = require('../middlewares/crushValidation');
 
 routes.get('/', async (req, res) => {
-  const crushes = await readCrushes();
-  if (!crushes.length) res.status(200).send([]);
-  res.status(200).send(crushes);
+  try {
+    const crushes = await readCrushes();
+    if (!crushes.length) res.status(200).send([]);
+    res.status(200).send(crushes);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 });
 
 routes.get('/search', auth, async (req, res) => {
@@ -23,14 +27,31 @@ routes.get('/search', auth, async (req, res) => {
 });
 
 routes.get('/:id', async (req, res) => {
-  const crushes = await readCrushes();
-  const { id } = req.params;
-  const foundCrush = crushes.find((crush) => crush.id === parseInt(id, 10));
-  if (!foundCrush) res.status(404).send({ message: 'Crush não encontrado' });
-  res.status(200).send(foundCrush);
+  try {
+    const crushes = await readCrushes();
+    const { id } = req.params;
+    const foundCrush = crushes.find((crush) => crush.id === parseInt(id, 10));
+    if (!foundCrush) res.status(404).send({ message: 'Crush não encontrado' });
+    res.status(200).send(foundCrush);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 });
 
 routes.use(auth);
+
+routes.delete('/:id', async (req, res) => {
+  try {
+    const crushes = await readCrushes();
+    const { id } = req.params;
+    const remainingCrushes = crushes.filter((crush) => crush.id !== parseInt(id, 10));
+    writeCrushes(remainingCrushes);
+    return res.status(200).send({ message: 'Crush deletado com sucesso' });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
 routes.use(crushValidation);
 routes.use(dateValidation);
 
