9576fcb42df5c1e3e7e4603eac6893646038b617
Erling <erlingjunior@gmail.com>
2021-04-11 21:55:26 -0300

    Requisito 1 com teste de array vazio, ok

--- routes/crush.js
+++ routes/crush.js
@@ -6,6 +6,9 @@ const app = express();
 const SUCCESS = 200;
 
 app.get('/', (_req, res) => {
+  if (data.length === []) {
+    return res.status(SUCCESS).send([]);
+  }
   res.status(SUCCESS).send(data);
 });
 
