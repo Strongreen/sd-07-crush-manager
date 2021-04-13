7d088ea032e2bad28eec493e35531c34db7916e7
Camila Valentim <camilaifpb@hotmail.com>
2021-04-13 09:09:24 -0300

    desafio

--- middlewares/tokenInvalido.js
+++ middlewares/tokenInvalido.js
@@ -3,7 +3,7 @@ const tokenInvalido = (req, res, next) => {
     const { authorization } = req.headers;
     const tokenRegex = (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/).test(authorization);
     if (!tokenRegex) {
-        res.status(401).send({
+        res.status(401).json({
             message: 'Token inválido',
         });
     }
