eec01baf992840a915bcde22a6cfabe87252f776
vini-lima <vini20.henrique@gmail.com>
2021-04-13 00:14:21 -0300

    Correção Lint 3

--- routes/login.js
+++ routes/login.js
@@ -10,17 +10,17 @@ router.post('/', (req, res) => {
   const regexEmail = /^(\s?[^\s,]+@[^\s,]+\.[^\s,]+\s?,)*(\s?[^\s,]+@[^\s,]+\.[^\s,]+)$/;
 
   if (req.body.length === 0) {
-    return res.status(400).send({ message: 'O campo "email" é obrigatório'})
-  };
+    return res.status(400).send({ message: 'O campo "email" é obrigatório' })
+  }
   if (!regexEmail.test(req.body.email)) {
-    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' })
-  };
+    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
   if (!req.body.password) {
-    return res.status(400).send({ message: 'O campo "password" é obrigatório'})
-  };
+    return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  }
   if (req.body.password.length < 6) {
-    return res.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' })
-  };
+    return res.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+  }
 
   const token = tokenGenerator();
   res.send({ token });
