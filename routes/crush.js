24fcb4432fb7c43bda3266e927d7e9863587842c
lisvalver <lisvalver.atte@gmail.com>
2021-04-13 09:08:14 -0300

    Desafio 02 - GET /crush/:id

--- routes/crush.js
+++ routes/crush.js
@@ -4,8 +4,22 @@ const fs = require('fs');
 const app = express();
 
 app.get('/', async (req, res) => {
-  const data = await fs.promises.readFile('./crush.json', 'utf-8');
-  res.status(200).send(JSON.parse(data));
+  const data = await JSON.parse(await fs.promises.readFile('./crush.json', 'utf-8'));
+  res.status(200).send(data);
+});
+
+app.get('/:id', async (req, res) => {
+  const data = await JSON.parse(await fs.promises.readFile('./crush.json', 'utf-8'));
+  const { id } = req.params;
+  const crushById = data.find((c) => c.id === parseInt(id, 10));
+
+  if (!crushById) {
+    res.status(404).send({
+      message: 'Crush não encontrado',
+    });
+  }
+
+  res.status(200).send(crushById);
 });
 
 module.exports = app;
