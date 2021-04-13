7dc7a92ea9fa51521ae167ebbfae6c6993b0050b
vini-lima <vini20.henrique@gmail.com>
2021-04-13 00:07:44 -0300

    Correção Lint 2

--- routes/login.js
+++ routes/login.js
@@ -9,18 +9,18 @@ const router = express.Router();
 router.post('/', (req, res) => {
   const regexEmail = /^(\s?[^\s,]+@[^\s,]+\.[^\s,]+\s?,)*(\s?[^\s,]+@[^\s,]+\.[^\s,]+)$/;
 
-  if (req.body.length === 0) { return res.status(400).send({
-    message: 'O campo "email" é obrigatório'
-  })};
-  if (!regexEmail.test(req.body.email)) { return res.status(400).send({
-    message: 'O "email" deve ter o formato "email@email.com"'
-  })};
-  if (!req.body.password) { return res.status(400).send({
-    message: 'O campo "password" é obrigatório'
-  })};
-  if (req.body.password.length < 6) { return res.status(400).send({
-    message: 'A "senha" deve ter pelo menos 6 caracteres'
-  })};
+  if (req.body.length === 0) {
+    return res.status(400).send({ message: 'O campo "email" é obrigatório'})
+  };
+  if (!regexEmail.test(req.body.email)) {
+    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' })
+  };
+  if (!req.body.password) {
+    return res.status(400).send({ message: 'O campo "password" é obrigatório'})
+  };
+  if (req.body.password.length < 6) {
+    return res.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' })
+  };
 
   const token = tokenGenerator();
   res.send({ token });
