633d15bbc34282db8353105e1ddd3d2e7b69a9b7
vini-lima <vini20.henrique@gmail.com>
2021-04-13 13:41:45 -0300

    Req 4 passando

--- routes/crush.js
+++ routes/crush.js
@@ -1,26 +1,25 @@
 const express = require('express');
-const rescue = require('express-rescue');
-const fs = require('fs').promises;
-const crush = require('../crush.json');
+const fs = require('fs');
 const { dataCrushMiddleware, tokenMiddleware } = require('../middlewares');
 
 const router = express.Router();
 
-router.get('/', rescue(async (_req, res) => {
+router.get('/', async (_req, res) => {
   try {
-    const responseCrush = await fs.readFile(`${__dirname}/../crush.json`);
+    const responseCrush = await fs.promises.readFile(`${__dirname}/../crush.json`);
     const responseCrushJSON = JSON.parse(responseCrush.toString('utf-8'));
     res.status(200).send(responseCrushJSON);
   } catch (error) {
     return error;
   }
-}));
+});
 
 router.get('/:id', (req, res) => {
+  const dataCrush = JSON.parse(fs.readFileSync(`${__dirname}/../crush.json`));
   const { id } = req.params;
   
-  if (crush[id - 1]) {
-    res.status(200).send(crush[id - 1]);
+  if (dataCrush[id - 1]) {
+    res.status(200).send(dataCrush[id - 1]);
   } else {
     res.status(404).send({
       message: 'Crush não encontrado',
@@ -31,17 +30,18 @@ router.get('/:id', (req, res) => {
 router.use(tokenMiddleware);
 router.use(dataCrushMiddleware);
 
-router.post('/', rescue(async (req, res) => {
+router.post('/', async (req, res) => {
+  const dataCrush = JSON.parse(fs.readFileSync(`${__dirname}/../crush.json`));
   const { name, age, date } = req.body;
-  const newCrushObject = { id: crush.length + 1, name, age, date };
-  const crushDocument = [...crush, newCrushObject];
+  const newCrushObject = { id: dataCrush.length + 1, name, age, date };
+  const crushDocument = [...dataCrush, newCrushObject];
 
   try {
-    await fs.writeFileSync(`${__dirname}/../crush.json`, JSON.stringify(crushDocument));
+    await fs.promises.writeFile(`${__dirname}/../crush.json`, JSON.stringify(crushDocument));
     return res.status(201).send(newCrushObject);
   } catch (error) {
     return error;
   }
-}));
+});
 
 module.exports = router;
