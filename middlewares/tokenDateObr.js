152e2ac8a16bb04b25c04b250285f38a1782d352
Camila Valentim <camilaifpb@hotmail.com>
2021-04-13 09:52:25 -0300

    desafio

--- middlewares/tokenDateObr.js
+++ middlewares/tokenDateObr.js
@@ -1,12 +1,17 @@
 const tokenDateObr = (req, res, next) => {
-    const { date, datedAt } = req.body;
-    const dateRegex = (/([0-2][0-9]|3[0-1])\/(0[0-9]|1[0-2])\/[0-9]{4}/).test(datedAt);
-    if (!date === date.datedAt || date.rate === 'undefined') {
+    const { date } = req.body;
+    if (date === undefined) {
+        return res
+          .status(400)
+       .send({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+      }
+    if (!date.datedAt || date.rate === 'undefined') {
         res.status(400).send({
             message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
         });
     }   
-    if (!dateRegex) {
+    const dateRegex = /([0-2][0-9]|3[0-1])\/(0[0-9]|1[0-2])\/[0-9]{4}/;
+    if (!dateRegex.test(date.datedAt)) {
         res.status(400).send({
             message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
         });
