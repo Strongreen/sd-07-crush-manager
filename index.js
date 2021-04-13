969faf0652260166fc3647826d385052fbf2e059
jvitorof <j.vitoroflores@gmail.com>
2021-04-12 22:24:08 -0300

    :triumph:

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ app.post('/login', (req, res) => {
   if (!re.test(email)) return res.status(400).send(message.error.formatEmail);
   if (!password) return res.status(400).send(message.error.mustPassword);
   if (!di.test(password)) return res.status(400).send(message.error.formatPassword);
-  req.headers.authorization = token(16);
-  return res.status(SUCCESS).send({ token: req.headers.authorization });
+  // req.headers.authorization = token(16);
+  return res.status(SUCCESS).send({ token: token(16) });
 });
 
 const authMiddleware = (req, res, next) => {
@@ -77,25 +77,82 @@ const authMiddleware = (req, res, next) => {
   next();
 };
 
-// // req4
-
 app.use(authMiddleware);
 
-app.post('/crush', async (req, res) => {
-  const { name, age, date } = req.body;
+const nameMiddleware = (req, res, next) => {
+  const { name } = req.body;  
   const minName = /\w{3}/;
-  const minAge = 18;
-  const formDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
-  const formRate = /^([0-5]{1})$/;
-
   if (!name) return res.status(400).send(message.error.noName);
   if (!minName.test(name)) return res.status(400).send(message.error.minLengtName);
+  next();
+};
+
+// req6
+app.delete(ROUTECRUSHID, async (req, res, next) => {
+  const { id } = req.params;
+  const data = JSON.parse(fs.readFileSync(CRUSH, 'utf8'));
+  const find = data.find((d) => d.id === parseInt(id, 10));
+  const index = data.indexOf(find);
+  if (index !== -1) data.splice(index, 1);
+  if (index === -1) {
+    return res.status(404).send(message.error.crushNotFound);
+  }
+
+  try {
+    await fs.promises.writeFile(CRUSH, JSON.stringify(data, null, 2));
+    res.status(SUCCESS).send({ message: 'Crush deletado com sucesso' });
+  } catch (error) {
+    throw new Error(error);
+  }
+  next();
+});
+
+// req4
+app.use(nameMiddleware);
+
+const ageMiddleware = (req, res, next) => {
+  const { age } = req.body;  
+  const minAge = 18;
   if (!age) return res.status(400).send(message.error.noAge);
   if (age < minAge) return res.status(400).send(message.error.mustAboveAge);
-  if (!date || !date.datedAt || !date.rate) return res.status(400).send(message.error.mustDate);
-  if (!formDate.test(date.datedAt)) return res.status(400).send(message.error.formatDate);
-  if (!formRate.test(date.rate)) return res.status(400).send(message.error.formatRate);
+  next();
+};
 
+app.use(ageMiddleware);
+
+const dateMiddleware = (req, res, next) => {
+  const { date } = req.body;
+  if (!date) return res.status(400).send(message.error.mustDate);
+  if (
+    !date.datedAt
+    || date.rate === null
+    || date.rate === undefined
+  ) return res.status(400).send(message.error.mustDate);
+  next();
+};
+
+app.use(dateMiddleware);
+
+const datedMiddleware = (req, res, next) => {
+  const { date } = req.body;  
+  const formDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+  if (!formDate.test(date.datedAt)) return res.status(400).send(message.error.formatDate);
+  next();
+};
+
+app.use(datedMiddleware);
+
+const rateMiddleware = (req, res, next) => {
+  const { date } = req.body;
+  const formRate = /^([1-5]{1})$/;
+  if (!formRate.test(date.rate)) return res.status(400).send(message.error.formatRate);
+  next();
+};
+
+app.use(rateMiddleware);
+
+app.post('/crush', async (req, res) => {
+  const { name, age, date } = req.body;  
   const data = JSON.parse(fs.readFileSync(CRUSH, 'utf8'));
   const size = data.length;
   data[size] = {
@@ -118,26 +175,6 @@ app.put(ROUTECRUSHID, async (req, res) => {
   const { id } = req.params;
   const { name, age, date } = req.body;
   
-  const minName = /\w{3}/;
-  const minAge = 18;
-  const formDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
-  const formRate = /^([1-5]{1})$/;
-
-  if (!name) return res.status(400).send(message.error.noName);
-  if (!minName.test(name)) return res.status(400).send(message.error.minLengtName);
-  if (!age) return res.status(400).send(message.error.noAge);
-  if (age < minAge) return res.status(400).send(message.error.mustAboveAge);
-  if (date && date.rate && !formRate.test(date.rate)) {
-    return res.status(400).send(message.error.formatRate);
-  }
-  if (date && date.datedAt && !formDate.test(date.datedAt)) {
-    return res.status(400).send(message.error.formatDate);
-  }
-  if (!date || (date && (!date.datedAt || !date.rate))) {
-    return res.status(400).send(message.error.mustDate);
-  }
-  // if (!date.datedAt || !date.rate) return res.status(400).send(message.error.mustDate);
-
   const data = JSON.parse(fs.readFileSync(CRUSH, 'utf8'));
   if (name) data[id - 1].name = name;
   if (age) data[id - 1].age = age;
@@ -149,27 +186,6 @@ app.put(ROUTECRUSHID, async (req, res) => {
   } catch (error) {
     throw new Error(error);
   }
-});
-
-// req6
-app.delete(ROUTECRUSHID, async (req, res, next) => {
-  const { id } = req.params;
-  const data = JSON.parse(fs.readFileSync(CRUSH, 'utf8'));
-
-  const find = data.find((d) => d.id === parseInt(id, 10));
-  const index = data.indexOf(find);
-  if (index !== -1) data.splice(index, 1);
-  if (index === -1) {
-    return res.status(404).send(message.error.crushNotFound);
-  }
-
-  try {
-    await fs.promises.writeFile(CRUSH, JSON.stringify(data, null, 2));
-    res.status(SUCCESS).send({ message: 'Crush deletado com sucesso' });
-  } catch (error) {
-    throw new Error(error);
-  }
-  next();
 });
 
 const errorMiddleware = (err, req, res, next) => {
