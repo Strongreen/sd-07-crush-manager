9cf184690a2e493ec01b4108c7aa086b697d511d
brendalima <brendalima@>
2021-04-15 18:42:30 -0300

    requirement n1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,24 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+const crush = './crush.json';
+
+// reads content from file crush.json
+const readCrushes = async () => {
+  try {
+    const content = await fs.readFile(crush, 'utf-8');
+    return JSON.parse(content);
+  }
+  catch (error) {
+    console.error(`Cannot read file`);
+  }
+}
+// returns an array of all crushes (requirement n1)
+app.get('/crush', async(req, res) => {
+  const allCrushes = await readCrushes();
+  res.status(SUCCESS).send(allCrushes);
 });
 
 app.listen(PORT, () => { console.log('Online'); });
