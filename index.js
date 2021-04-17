e132cedbea7cb4181fe5f74f332ff8f839f10337
brendalima <brendalima@>
2021-04-17 19:53:54 -0300

    requirement n3

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const crypto = require('crypto');
+
+// from https://nodejs.org/api/crypto.html#crypto_crypto_randombytes_size_callback
+// const {
+//   randomBytes,
+// } = import('crypto');
 
 const app = express();
 app.use(bodyParser.json());
 
 const SUCCESS = 200;
 const NOT_FOUND = 404;
+const BAD_REQUEST = '400';
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -15,6 +22,32 @@ app.get('/', (_request, response) => {
 });
 
 const crushJSON = './crush.json';
+
+// function from https://ui.dev/validate-email-address-javascript/
+const emailIsValid = (email) => {
+  let message;
+  if (!email) {
+    message = 'O campo "email" é obrigatório';
+  } else if (!(/\S+@\S+\.\S+/.test(email))) {
+    message = 'O "email" deve ter o formato "email@email.com"';
+  }
+  return message;
+};
+
+const passwordIsValid = (password) => {
+  const MIN_PASSWORD_LENGTH = 6;
+  let message;
+  if (!password) {
+    message = 'O campo "password" é obrigatório';
+  } else if (password.toString().length < MIN_PASSWORD_LENGTH) {
+    message = 'A "senha" deve ter pelo menos 6 caracteres';
+  }
+  return message;
+};
+
+const getToken = () => 
+   crypto.randomBytes(8).toString('hex');
+  // return randomBytes(8).toString('hex');
 
 // reads content from file crush.json
 const readCrushs = async () => {
@@ -42,6 +75,20 @@ app.get('/crush/:id', async (req, res) => {
       message: 'Crush não encontrado',
     });
   }
+});
+
+// creates post for login (requirement n3)
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const emailValidation = emailIsValid(email);
+  const passwordValidation = passwordIsValid(password);
+  if (emailValidation) {
+    return res.status(BAD_REQUEST).send({ message: `${emailValidation}` });
+  }
+  if (passwordValidation) {
+    return res.status(BAD_REQUEST).send({ message: `${passwordValidation}` });
+  } 
+    return res.status(SUCCESS).send({ token: `${getToken()}` });
 });
 
 app.listen(PORT, () => { console.log('Online'); });
