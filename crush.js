eb300b39ec21a71ae690f85469bdf26c245d2fcd
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-04-18 13:54:42 -0300

    requirement 2

--- crush.js
+++ crush.js
@@ -14,4 +14,18 @@ app.get('/', async (_req, res) => {
   res.status(200).json(JSON.parse(await crushData()));
 });
 
+const crushIDs = async (id) => {
+  const findID = JSON.parse(await crushData());
+  return findID.find((crushInfos) => crushInfos.id.toString() === id);
+};
+
+app.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const IDmatch = await crushIDs(id);
+  if (!IDmatch) {
+    return res.status(404).json({ message: 'Crush não encontrado' });
+  }
+  return res.status(200).json(IDmatch);
+});
+
 module.exports = app;
