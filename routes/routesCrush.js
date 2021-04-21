9b1039ba68713a26656aa943139b13fc997819cc
Felipe Farina <felipe@pop-os.localdomain>
2021-04-21 13:32:16 -0300

    Initialize project, requisit 2

--- routes/routesCrush.js
+++ routes/routesCrush.js
@@ -15,4 +15,19 @@ routes.get('/crush', (req, res) => {
   res.status(200).send(data);
 });
 
+routes.route('/crush/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+
+    const file = fs.readFileSync(FILE);
+    const dataString = file.toString('utf-8');
+    const data = JSON.parse(dataString);
+
+    const matchCrush = data.find((crush) => crush.id === Number(id));
+
+    if (!matchCrush) return res.status(404).send({ message: 'Crush não encontrado' });
+
+    res.status(200).send(matchCrush);
+  });
+
 module.exports = routes;
