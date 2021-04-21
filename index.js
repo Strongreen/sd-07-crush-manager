b76e82a5dec6df44f96c9d1ee587543b5aefcde4
Felipe Farina <felipe@pop-os.localdomain>
2021-04-21 13:59:47 -0300

    requisit 3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const routesCrush = require('./routes/routesCrush');
+const routesLogin = require('./routes/routesLogin');
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,5 +17,6 @@ app.get('/', (_request, response) => {
 });
 
 app.use(routesCrush);
+app.use(routesLogin);
 
 app.listen(PORT, () => { console.log('Online'); });
