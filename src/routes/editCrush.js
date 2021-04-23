656b93fa7be80d5a235b36888e2a0244214bc378
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-04-22 22:27:06 -0300

    Exercise 5 - id to number

--- src/routes/editCrush.js
+++ src/routes/editCrush.js
@@ -8,11 +8,13 @@ const route = express.Router();
 const dataDirectory = path.join(__dirname, '../../crush.json');
 
 route.put('/crush/:id', tokenMiddleware, dataValidations, async (req, res) => {
+  const { name, age, date } = req.body;
+  const { id } = req.params;
   let crushes = await fs.readFile(dataDirectory);
   crushes = JSON.parse(crushes);
   const crush = crushes.find((element) => element.id === +req.body.id);
   const index = crushes.indexOf(crush);
-  crushes[index] = { ...req.body };
+  crushes[index] = { name, age, id: +id, date: { datedAt: date.datedAt, rate: date.rate } };
   try {
     await fs.writeFile(dataDirectory, JSON.stringify(crushes));
     const resp = crushes[index];
