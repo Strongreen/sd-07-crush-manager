9923c92d4fcfe3cdbe6b44649a77fa45a681196e
BrunoNogueiraBruno <nugobruno@gmail.com>
2021-04-23 23:56:29 -0300

    linttttttt

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const { response } = require('express');
 const { OK, NOT_FOUND, BAD_REQUEST, UNAUTHORIZED, CREATED } = require('./httpCode');
 const {
   notFound,
