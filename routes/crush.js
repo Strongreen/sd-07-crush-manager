4a31bc65302dcc8cd38bb913bdac3b217ac40da6
Thays Costa <thayscosta3@gmail.com>
2021-04-26 22:14:59 -0300

    Req. 2 vem ai

--- routes/crush.js
+++ routes/crush.js
@@ -5,18 +5,35 @@ const crush = express.Router();
 
 function readCrushesFile() {
   return fs.readFile(`${__dirname}/../crush.json`, 'utf-8')
-    .then((content) => JSON.parse(content))
-    .catch((err) => console.log(err.message));
+  .then((content) => JSON.parse(content))
+  .catch((err) => console.log(err.message));
 }
 
 const SUCCESS = 200;
 
-crush.get('/', async (_request, response) => {
+crush.get('/:id', async (request, response) => {
+  try {
+    console.log(request.params);
+    const result = await readCrushesFile();
+    const crushId = result.find((crushData) => crushData.id === Number(request.params.id));
+    if (crushId) {
+      return response.status(SUCCESS).send(crushId);
+    }
+    const message = {
+      message: 'Crush não encontrado',
+    };
+    return response.status(404).send(message);
+  } catch (err) {
+    response.status(500).send(err.message);
+  }
+});
+
+crush.get('/', async (request, response) => {
   try {
     const result = await readCrushesFile();
     response.status(SUCCESS).send(result);
   } catch (err) {
-    response.status(300).send('Este é o erro');
+    response.status(500).send(err.message);
   }
 });
 
