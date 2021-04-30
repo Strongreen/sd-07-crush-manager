48d69291afacf2125cc4228aba3b8336fa40757a
Eduardo Pizorno <pizorno@gmial.com>
2021-04-30 10:34:38 -0300

    Mais uma tentativa do projeto finalizado!

---
+++ routers/crushRouter.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const fs = require('fs');
+const crypto = require('crypto');
+const crushModels = require('../models/crushModels');
+
+const router = express.Router();
+
+const crushJson = 'crush.json';
+const crushInit = '/crush';
+const crushRoute = '/crush/:id';
+
+/* ---------- REQUISITO 1 ---------- */
+router.get(crushInit, (_req, res) => {
+  const data = JSON.parse(fs.readFileSync(crushJson, 'utf8'));
+  if (!data.length) return res.status(200).json([]);
+  res.status(200).json(data);
+});
+
+/* ---------- REQUISITO 7 -----------*/
+router.get('/crush/search', (req, res) => {
+  const data = JSON.parse(fs.readFileSync(crushJson, 'utf8'));
+  const { authorization } = req.headers;
+  const { q: search } = req.query;
+  try {
+    crushModels.validToken(authorization);
+    const crushFiltered = data.filter((crush) =>
+      crush.name.toUpperCase().includes(search.toUpperCase()));
+    if (!search || search === '') return res.status(200).json(router.get(crushInit));
+    return res.status(200).json(crushFiltered);
+  } catch (error) {
+    if (error.message.includes('Token')) {
+      return res.status(401).json({ message: error.message });
+    }
+    res.status(400).json({ message: error.message });
+  }
+});
+
+/* ---------- REQUISITO 2 -----------*/
+router.get(crushRoute, (req, res) => {
+  const data = JSON.parse(fs.readFileSync(crushJson, 'utf8'));
+  const { id } = req.params;
+  const crushFind = data.find((crush) => crush.id === +id);
+  if (!crushFind) return res.status(404).json({ message: 'Crush não encontrado' });
+  res.status(200).json(crushFind);
+});
+
+/* ----------- REQUISITO 3 ---------- */
+router.post('/login', (req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  const { email, password } = req.body;
+  try {
+    const validEmailResult = crushModels.validEmail(email);
+    if (validEmailResult.error) {
+      return res.status(400).json({ message: validEmailResult.message });
+    }
+    const validPasswordResult = crushModels.validPass(password);
+    if (validPasswordResult.error) {
+      return res.status(400).json({ message: validPasswordResult.message });
+    }
+    return res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).send({ message: 'ta zuado o role' });
+  }
+});
+
+/* ---------- REQUISITO 4 ---------- */
+router.post(crushInit, (req, res) => {
+  const data = JSON.parse(fs.readFileSync(crushJson, 'utf8'));
+  const { authorization } = req.headers;
+  const { name, age, date } = req.body;
+  const element = req.body;
+  try {
+    crushModels.validToken(authorization);
+    crushModels.validForAll(element);
+    data.push({ name, age, id: data.length + 1, date });
+    fs.writeFileSync(`${__dirname}/../crush.json`, JSON.stringify(data));
+    res.status(201).json(data[data.length - 1]);
+  } catch (error) {
+    if (error.message.includes('Token')) return res.status(401).json({ message: error.message });
+    res.status(400).json({ message: error.message });
+  }
+});
+/* data[data.length - 1] */
+
+/* ----------- REQUISITO 5 ---------- */
+router.put(crushRoute, (req, res) => {
+  const data = JSON.parse(fs.readFileSync(crushJson, 'utf8'));
+  const element = req.body;
+  const { authorization } = req.headers;
+  const { id } = req.params;
+  const { name, age, date } = req.body;
+  try {
+    crushModels.validToken(authorization);
+    crushModels.validForAll(element);
+    data[id - 1].name = name;
+    data[id - 1].age = age;
+    data[id - 1].date = date;
+    fs.writeFileSync(`${__dirname}/../crush.json`, JSON.stringify(data));
+    return res.status(200).json(data[id - 1]);
+  } catch (error) {
+    if (error.message.includes('Token')) return res.status(401).json({ message: error.message });
+    res.status(400).json({ message: error.message });
+  }
+});
+
+/* ----------- REQUISITO 6 ---------- */
+router.delete(crushRoute, (req, res) => {
+  const data = JSON.parse(fs.readFileSync(crushJson, 'utf8'));
+  const { authorization } = req.headers;
+  const { id } = req.params;
+  crushModels.validToken(authorization);
+  const index = Number(id - 1);
+  data.splice(index, 1);
+  try {
+    fs.writeFileSync(`${__dirname}/../crush.json`, JSON.stringify(data));
+    return res.status(200).json({ message: 'Crush deletado com sucesso' });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+router.use((err, _req, res, _next) => {
+  res.status(401).json({ message: err.message });
+});
+
+module.exports = router;
