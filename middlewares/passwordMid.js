d5b569170e6eb5cc3deeb45454bb915a74bfafe6
Arthur de Sa <arthurdesa94@gmail.com>
2021-05-05 10:58:58 -0300

    refatorando o login e adicionando midwares de email, password e token

--- middlewares/passwordMid.js
+++ middlewares/passwordMid.js
@@ -1,17 +1,17 @@
-// const passwordMid = (req, res, next) => {
-//   const { password } = req.body;
+const passwordMid = (req, res, next) => {
+  const { password } = req.body;
 
-//   if (!password || password === '') {
-//     res.status(400).send({
-//       message: 'O campo "password" é obrigatório',
-//     });
-//   }
-//   if (password.length < 6) {
-//     return res.status(400).json({
-//       message: 'A "senha" deve ter pelo menos 6 caracteres',
-//     });
-//   }
-//   next();
-// };
+  if (!password || password === '') {
+    res.status(400).send({
+      message: 'O campo "password" é obrigatório',
+    });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: 'A "senha" deve ter pelo menos 6 caracteres',
+    });
+  }
+  next();
+};
 
-// module.exports = passwordMid;
+module.exports = passwordMid;
--- routes/login.js
+++ routes/login.js
@@ -1,40 +1,14 @@
 const express = require('express');
-const randomToken = require('random-token');
 
-const app = express();
+const router = express.Router();
 
-const checkEmail = (email) => {
-  const regexEmail = new RegExp('.+@[A-z]+[.]com');
+const midwares = require('../middlewares');
 
-  if (email === undefined || email.length === 0) {
-    throw new Error('O campo "email" é obrigatório');
-  } else if (!regexEmail.test(email)) {
-    throw new Error('O "email" deve ter o formato "email@email.com"');
-  }
-};
+router.post(
+  '/',
+  midwares.emailMid,
+  midwares.passwordMid,
+  midwares.requestTokenMid,
+);
 
-const checkPassword = (password) => {
-  const regexPassword = new RegExp('.{6}');
-
-  if (!password || password.length === 0) {
-    throw new Error('O campo "password" é obrigatório');
-  } else if (!regexPassword.test(password)) {
-    throw new Error('A "senha" deve ter pelo menos 6 caracteres');
-  }
-};
-
-app.post('/', (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    checkEmail(email);
-    checkPassword(password);
-
-    const token = randomToken(16);
-    res.send({ token });
-  } catch (error) {
-    res.status(400).send({ message: error.message });
-  }
-});
-
-module.exports = app;
+module.exports = router;
