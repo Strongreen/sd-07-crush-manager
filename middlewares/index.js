3456a97ce1626ebf66466e5fb4ceec3fe812f45a
Fabio Camargos <fabiocmmcm@gmail.com>
2021-05-09 23:55:19 -0300

    refactoring Code

---
+++ middlewares/index.js
@@ -0,0 +1,19 @@
+const validateEmail = require('./validateEmail');
+const validatePassword = require('./validatePassword');
+const validateToken = require('./validateToken');
+const validadeAge = require('./validateAge');
+const validadeDate = require('./validadeDate');
+const validateDateAt = require('./validadeDateAt');
+const validateName = require('./validateName');
+const validateRate = require('./validateRate');
+
+module.exports = {
+  validateToken,
+  validateEmail,
+  validatePassword,
+  validadeAge,
+  validadeDate,
+  validateDateAt,
+  validateName,
+  validateRate,
+};
---
+++ middlewares/validateAge.js
@@ -0,0 +1,16 @@
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  console.log(age);
+
+  if (age === undefined || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (parseInt(age, 10) <= 18) {
+    return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+  }
+
+  next();
+};
+
+module.exports = validateAge; 
