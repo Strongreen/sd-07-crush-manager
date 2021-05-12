6ff34833aa2e8050fbdcb7123017331afac3b4c1
André Ferreira <andreferreirap1@gmail.com>
2021-05-12 06:27:37 -0300

    Requisito 5 | Colocando validações como parâmetro apó a rota, adicionando validação do token | ESLint

--- index.js
+++ index.js
@@ -118,7 +118,8 @@ app.put(
       age: crushs[key].age,
       date: crushs[key].date });
   } catch (error) { res.status(400).send({ message: error.message }); }
-}));
+  }),
+);
 
 // app.get('/crush/search', middlewareLogin, (req, res) => { // 7
 //   const crushs = JSON.parse(fs.readFileSync(fileCrushs), 'utf-8');
