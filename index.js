d5a747fe86360bc08803b5329e4b9564c45ce623
André Ferreira <andreferreirap1@gmail.com>
2021-05-12 02:58:31 -0300

    Requisito 2 | Mudando condição if

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ app.use(bodyParser.json());
 const SUCCESS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(SUCCESS).send();
-});
-
 // 1
 app.get('/crush', (req, res) => {
   const crushs = JSON.parse(fs.readFileSync(fileCrushs), 'utf-8');  
@@ -27,13 +22,18 @@ app.get('/crush/:id', (req, res) => {
   const { id } = req.params;
   const crushId = crushs.find((element) => element.id === parseInt(id, 10));
 
-  if (crushId === true) {
+  if (crushId) {
     res.status(SUCCESS).send(crushId);
   }
 
   res.status(404).send(
     { message: 'Crush não encontrado' },
   );
+});
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(SUCCESS).send();
 });
 
 app.listen(PORT, () => { console.log('Online'); });
